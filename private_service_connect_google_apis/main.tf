9b73cf0e93e3373be6b1bb6bcba011ddb0431f9f
The Magician <magic-modules@google.com>
2021-07-28 16:49:24 -0500

    Move CGC region tags to template file (#4999) (#2)
    * Move CGC region tags to template file
    
    * make rubocop happy
    
    * more rubocop
    
    * Update private_service_connect_google_apis.tf.erb
    
    * skip oics link for examples with test_env_vars
    
    * extra condition
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ private_service_connect_google_apis/main.tf
@@ -0,0 +1,42 @@
+# [START vpc_subnet_private_access]
+resource "google_compute_network" "network" {
+  provider      = google-beta
+  project       = "my-project-name"
+  name          = "my-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "vpc_subnetwork" {
+  provider                 = google-beta
+  project                  = google_compute_network.network.project
+  name                     = "my-subnetwork"
+  ip_cidr_range            = "10.2.0.0/16"
+  region                   = "us-central1"
+  network                  = google_compute_network.network.id
+  private_ip_google_access = true
+}
+# [END vpc_subnet_private_access]
+
+# [START compute_internal_ip_private_access]
+resource "google_compute_global_address" "default" {
+  provider      = google-beta
+  project       = google_compute_network.network.project
+  name          = "global-psconnect-ip"
+  address_type  = "INTERNAL"
+  purpose       = "PRIVATE_SERVICE_CONNECT"
+  network       = google_compute_network.network.id
+  address       = "100.100.100.106"
+}
+# [END compute_internal_ip_private_access]
+
+# [START compute_forwarding_rule_private_access]
+resource "google_compute_global_forwarding_rule" "default" {
+  provider      = google-beta
+  project       = google_compute_network.network.project
+  name          = "globalrule"
+  target        = "all-apis"
+  network       = google_compute_network.network.id
+  ip_address    = google_compute_global_address.default.id
+  load_balancing_scheme = ""
+}
+# [END compute_forwarding_rule_private_access]
