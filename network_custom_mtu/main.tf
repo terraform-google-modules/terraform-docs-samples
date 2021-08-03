5ec78d406e8b8a564c84fbd8b513f7c79c15d3fb
The Magician <magic-modules@google.com>
2021-08-03 16:34:00 -0500

    add vpc_auto_create sample (#5023) (#3)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ network_custom_mtu/main.tf
@@ -0,0 +1,8 @@
+# [START vpc_auto_create]
+resource "google_compute_network" "vpc_network" {
+  project                 = "my-project-name"
+  name                    = "vpc-network"
+  auto_create_subnetworks = true
+  mtu                     = 1460
+}
+# [END vpc_auto_create]
