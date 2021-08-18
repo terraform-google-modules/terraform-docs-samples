9f69360f2b345eae9e90eb5d1e1064732b081638
Modular Magician <magic-modules@google.com>
2021-08-18 15:32:00 +0000

    add vpc_firewall_create sample (#5024)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ firewall_with_target_tags/main.tf
@@ -0,0 +1,14 @@
+# [START vpc_firewall_create]
+resource "google_compute_firewall" "rules" {
+  project     = "my-project-name"
+  name        = "my-firewall-rule"
+  network     = "default"
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["80", "8080", "1000-2000"]
+  }
+  target_tags = ["web"]
+}
+# [END vpc_firewall_create]
