8bb35f99df38c77a8f65055db7cf421be4cd102c
Modular Magician <magic-modules@google.com>
2022-02-11 20:47:18 +0000

    Add tags for cloud.google.com pages (#5697)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ target_http_proxy_basic/main.tf
@@ -0,0 +1,42 @@
+# [START cloudloadbalancing_target_http_proxy_basic]
+resource "google_compute_target_http_proxy" "default" {
+  name    = "test-proxy"
+  url_map = google_compute_url_map.default.id
+}
+
+resource "google_compute_url_map" "default" {
+  name            = "url-map"
+  default_service = google_compute_backend_service.default.id
+
+  host_rule {
+    hosts        = ["mysite.com"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = google_compute_backend_service.default.id
+
+    path_rule {
+      paths   = ["/*"]
+      service = google_compute_backend_service.default.id
+    }
+  }
+}
+
+resource "google_compute_backend_service" "default" {
+  name        = "backend-service"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_http_health_check.default.id]
+}
+
+resource "google_compute_http_health_check" "default" {
+  name               = "http-health-check"
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
+}
+# [END cloudloadbalancing_target_http_proxy_basic]
---
+++ url_map_basic/main.tf
@@ -0,0 +1,76 @@
+# [START trafficdirector_url_map_basic]
+resource "google_compute_url_map" "urlmap" {
+  name        = "urlmap"
+  description = "a description"
+
+  default_service = google_compute_backend_bucket.static.id
+
+  host_rule {
+    hosts        = ["mysite.com"]
+    path_matcher = "mysite"
+  }
+
+  host_rule {
+    hosts        = ["myothersite.com"]
+    path_matcher = "otherpaths"
+  }
+
+  path_matcher {
+    name            = "mysite"
+    default_service = google_compute_backend_bucket.static.id
+
+    path_rule {
+      paths   = ["/home"]
+      service = google_compute_backend_bucket.static.id
+    }
+
+    path_rule {
+      paths   = ["/login"]
+      service = google_compute_backend_service.login.id
+    }
+
+    path_rule {
+      paths   = ["/static"]
+      service = google_compute_backend_bucket.static.id
+    }
+  }
+
+  path_matcher {
+    name            = "otherpaths"
+    default_service = google_compute_backend_bucket.static.id
+  }
+
+  test {
+    service = google_compute_backend_bucket.static.id
+    host    = "hi.com"
+    path    = "/home"
+  }
+}
+
+resource "google_compute_backend_service" "login" {
+  name        = "login"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  health_checks = [google_compute_http_health_check.default.id]
+}
+
+resource "google_compute_http_health_check" "default" {
+  name               = "health-check"
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
+}
+
+resource "google_compute_backend_bucket" "static" {
+  name        = "static-asset-backend-bucket"
+  bucket_name = google_storage_bucket.static.name
+  enable_cdn  = true
+}
+
+resource "google_storage_bucket" "static" {
+  name     = "static-asset-bucket"
+  location = "US"
+}
+# [END trafficdirector_url_map_basic]
