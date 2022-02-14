e0f7e0c8e1cd75da387a6c7763ad5542e7ad1d6c
Modular Magician <magic-modules@google.com>
2022-02-14 23:22:44 +0000

    Added example for creating a Postgres instance (#5706)
    Co-authored-by: Cameron Thornton <camthornton@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ sql_database_instance_postgres/main.tf
@@ -0,0 +1,11 @@
+# [START cloud_sql_postgres_instance_80_db_n1_s2]
+resource "google_sql_database_instance" "instance" {
+  name             = "postgres-instance"
+  region           = "us-central1"
+  database_version = "POSTGRES_12"
+  settings {
+    tier = "db-custom-2-7680"
+  }
+  deletion_protection =  "true"
+}
+# [END cloud_sql_postgres_instance_80_db_n1_s2]
