138259f6a7a29710e84a2096a934e6e85dc4b214
The Magician <magic-modules@google.com>
2022-03-17 11:23:36 -0500

    Added examples for Cloud SQL backup retention (#5809) (#37)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ sql_postgres_instance_backup_retention/main.tf
@@ -0,0 +1,18 @@
+# [START cloud_sql_postgres_instance_backup_retention]
+resource "google_sql_database_instance" "default" {
+  name             = "postgres-instance-backup-retention"
+  region           = "us-central1"
+  database_version = "POSTGRES_12"
+  settings {
+    tier = "db-custom-2-7680"
+    backup_configuration {
+      enabled                        = true
+      backup_retention_settings {
+        retained_backups               = 365
+        retention_unit                 = "COUNT"
+      }
+    }
+  }
+  deletion_protection =  "true"
+}
+# [END cloud_sql_postgres_instance_backup_retention]
---
+++ sql_sqlserver_instance_backup_retention/main.tf
@@ -0,0 +1,19 @@
+# [START cloud_sql_sqlserver_instance_backup_retention]
+resource "google_sql_database_instance" "default" {
+  name             = "sqlserver-instance-backup-retention"
+  region           = "us-central1"
+  database_version = "SQLSERVER_2017_STANDARD"
+  root_password = "INSERT-PASSWORD-HERE"
+  settings {
+    tier = "db-custom-2-7680"
+    backup_configuration {
+      enabled                        = true
+      backup_retention_settings {
+        retained_backups               = 365
+        retention_unit                 = "COUNT"
+      }
+    }
+  }
+  deletion_protection =  "true"
+}
+# [END cloud_sql_sqlserver_instance_backup_retention]
