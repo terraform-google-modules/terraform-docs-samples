9d81fc6468f5640e4827ee724a067f2da9ddedae
Modular Magician <magic-modules@google.com>
2022-04-15 15:08:08 +0000

    Added nested region tags to Cloud SQL private IP examples (#5945)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- sql_postgres_instance_private_ip/main.tf
+++ sql_postgres_instance_private_ip/main.tf
@@ -1,9 +1,13 @@
 # [START cloud_sql_postgres_instance_private_ip]
+
+# [START vpc_postgres_instance_private_ip_network]
 resource "google_compute_network" "private_network" {
   name                    = "private-network"
   auto_create_subnetworks = "false"
 }
+# [END vpc_postgres_instance_private_ip_network]
 
+# [START vpc_postgres_instance_private_ip_address]
 resource "google_compute_global_address" "private_ip_address" {
   name          = "private-ip-address"
   purpose       = "VPC_PEERING"
@@ -11,17 +15,21 @@ resource "google_compute_global_address" "private_ip_address" {
   prefix_length = 16
   network       = google_compute_network.private_network.id
 }
+# [END vpc_postgres_instance_private_ip_address]
 
+# [START vpc_postgres_instance_private_ip_service_connection]
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.private_network.id
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
+# [END vpc_postgres_instance_private_ip_service_connection]
 
+# [START cloud_sql_postgres_instance_private_ip_instance]
 resource "google_sql_database_instance" "default" {
   name             = "private-ip-sql-instance"
   region           = "us-central1"
-  database_version = "POSTGRES_12"
+  database_version = "POSTGRES_14"
 
   depends_on = [google_service_networking_connection.private_vpc_connection]
 
@@ -34,4 +42,6 @@ resource "google_sql_database_instance" "default" {
   }
   deletion_protection = "false"
 }
+# [END cloud_sql_postgres_instance_private_ip_instance]
+
 # [END cloud_sql_postgres_instance_private_ip]
