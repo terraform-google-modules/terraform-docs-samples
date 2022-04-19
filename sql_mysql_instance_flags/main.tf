b3d0fea64a066b1c9486f06654f447c098d1addf
The Magician <magic-modules@google.com>
2022-04-19 12:49:42 -0500

    Added Cloud SQL flag examples (#5943) (#55)
    * Added Cloud SQL flags and user examples
    
    * Changed name to id to fix Reference to undeclared resource error
    
    * fixed references
    
    * fixed flags
    
    * Removed the user part because it's causing errors
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ sql_mysql_instance_flags/main.tf
@@ -0,0 +1,24 @@
+# [START cloud_sql_mysql_instance_flags]
+resource "google_sql_database_instance" "instance" {
+  database_version = "MYSQL_8_0"
+  name             = "mysql-instance"
+  region           = "us-central1"
+  settings {
+    database_flags {
+      name  = "general_log"
+      value = "on"
+    }
+    database_flags {
+      name  = "skip_show_database"
+      value = "on"
+    }
+    database_flags {
+      name  = "wait_timeout"
+      value = "200000"
+    }
+    disk_type             = "PD_SSD"
+    tier         = "db-n1-standard-2"
+  }
+  deletion_protection = "true"
+}
+# [END cloud_sql_mysql_instance_flags]
