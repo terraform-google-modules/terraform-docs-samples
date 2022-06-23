05abf69dbf81d007a97944096ab18ae359d7a371
The Magician <magic-modules@google.com>
2022-06-23 14:11:24 -0700

    feat: add new Cloud Run tag samples (#6135) (#82)
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ cloud_run_service_add_tag/main.tf
@@ -0,0 +1,21 @@
+# [START cloud_run_service_add_tag]
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {}
+
+  traffic {
+    percent       = 100
+    # This revision needs to already exist
+    revision_name = "cloudrun-srv-green"
+  }
+
+  traffic {
+    # Deploy new revision with 0% traffic
+    percent       = 0
+    revision_name = "cloudrun-srv-blue"
+    tag           = "tag-name"
+  }
+}
+# [END cloud_run_service_add_tag]
