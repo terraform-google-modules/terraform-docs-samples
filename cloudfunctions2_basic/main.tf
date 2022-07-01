ea420acb318bcaff3f20c571a8d6a9c7a3eccbdb
The Magician <magic-modules@google.com>
2022-07-01 09:10:41 -0700

    fix(functions) Add region tags to Cloud Functions samples (#6191) (#88)
    * Add region tags to Cloud Functions samples
    
    * add back provider
    
    * use pubsub function for test
    
    * Update cloudfunctions2_full.tf.erb
    
    * update test variables
    
    * update SA name
    
    * address review comments from #6170
    
    replaces #6170
    
    Co-authored-by: averikitsch <akitsch@google.com>
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Co-authored-by: averikitsch <akitsch@google.com>

---
+++ cloudfunctions2_basic/main.tf
@@ -0,0 +1,39 @@
+# [START functions_v2_basic]
+resource "google_storage_bucket" "bucket" {
+  provider = google-beta
+  name     = "cloudfunctions2-function-bucket"  # Every bucket name must be globally unique
+  location = "US"
+  uniform_bucket_level_access = true
+}
+ 
+resource "google_storage_bucket_object" "object" {
+  provider = google-beta
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "path/to/index.zip"  # Add path to the zipped function source code
+}
+ 
+resource "google_cloudfunctions2_function" "terraform-test2" {
+  provider = google-beta
+  name = "test-function"
+  location = "us-central1"
+  description = "a new function"
+ 
+  build_config {
+    runtime = "nodejs16"
+    entry_point = "helloHttp"  # Set the entry point 
+    source {
+      storage_source {
+        bucket = google_storage_bucket.bucket.name
+        object = google_storage_bucket_object.object.name
+      }
+    }
+  }
+ 
+  service_config {
+    max_instance_count  = 1
+    available_memory    = "256M"
+    timeout_seconds     = 60
+  }
+}
+# [END functions_v2_basic]
---
+++ cloudfunctions2_full/main.tf
@@ -0,0 +1,68 @@
+# [START functions_v2_full]
+resource "google_service_account" "account" {
+  provider = google-beta
+  account_id = "s-a"
+  display_name = "Test Service Account"
+}
+
+resource "google_pubsub_topic" "sub" {
+  provider = google-beta
+  name = "pub-sub"
+}
+
+resource "google_storage_bucket" "bucket" {
+  provider = google-beta
+  name     = "cloudfunctions2-function-bucket"  # Every bucket name must be globally unique
+  location = "US"
+  uniform_bucket_level_access = true
+}
+ 
+resource "google_storage_bucket_object" "object" {
+  provider = google-beta
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "path/to/index.zip"  # Add path to the zipped function source code
+}
+ 
+resource "google_cloudfunctions2_function" "terraform-test" {
+  provider = google-beta
+  name = "test-function"
+  location = "us-central1"
+  description = "a new function"
+ 
+  build_config {
+    runtime = "nodejs16"
+    entry_point = "helloPubSub"  # Set the entry point 
+    environment_variables = {
+        BUILD_CONFIG_TEST = "build_test"
+    }
+    source {
+      storage_source {
+        bucket = google_storage_bucket.bucket.name
+        object = google_storage_bucket_object.object.name
+      }
+    }
+  }
+ 
+  service_config {
+    max_instance_count  = 3
+    min_instance_count = 1
+    available_memory    = "256M"
+    timeout_seconds     = 60
+    environment_variables = {
+        SERVICE_CONFIG_TEST = "config_test"
+    }
+    ingress_settings = "ALLOW_INTERNAL_ONLY"
+    all_traffic_on_latest_revision = true
+    service_account_email = google_service_account.account.email
+  }
+
+  event_trigger {
+    trigger_region = "us-central1"
+    event_type = "google.cloud.pubsub.topic.v1.messagePublished"
+    pubsub_topic = google_pubsub_topic.sub.id
+    retry_policy = "RETRY_POLICY_RETRY"
+    service_account_email = google_service_account.account.email
+  }
+}
+# [END functions_v2_full]
