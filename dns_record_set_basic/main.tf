0fc6230c7dba0d926ee31fcdf56410e7154415e9
The Magician <magic-modules@google.com>
2022-07-06 09:59:54 -0700

    feat: DNS samples record sets (#6173) (#89)
    * Add DNS Resource Record Sets Example
    
    * Update terraform.yaml
    
    * make dns_managed_zone  to be primary resource
    
    * Update vars
    
    * Omit duplicate line
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ dns_record_set_basic/main.tf
@@ -0,0 +1,17 @@
+# [START dns_record_set_basic]
+resource "google_dns_managed_zone" "parent-zone" {
+  provider    = "google-beta"
+  name        = "sample-zone"
+  dns_name    = "sample-zone.hashicorptest.com."
+  description = "Test Description"
+}
+
+resource "google_dns_record_set" "default" {
+  provider     = "google-beta"
+  managed_zone = google_dns_managed_zone.parent-zone.name
+  name         = "test-record.sample-zone.hashicorptest.com."
+  type         = "A"
+  rrdatas      = ["10.0.0.1", "10.1.0.1"]
+  ttl          = 86400
+}
+# [END dns_record_set_basic]
