3806336a80b0ec17572e9d13e0afb8cf45f6b271
Modular Magician <magic-modules@google.com>
2022-07-06 19:16:41 +0000

    Compute reservation terraform sample for C.G.C (#6217)
    * Compute reservation terraform sample for C.G.C
    
    * Removed Beta version to use only GA versions
    
    * updated to resolve possible blocker
    
    * Updated the erb file
    
    * Corrected for test runs
    
    * Added correct project env parameter
    
    * Removed additional spaces
    
    * Renamed file for tests to run
    
    * Update terraform.yaml
    
    Added skip_import_test config for workaround
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ compute_reservation/main.tf
@@ -0,0 +1,20 @@
+# [START compute_reservation_create_local_reservation]
+
+resource "google_compute_reservation" "gce_reservation_local" {
+  name = "gce-reservation-local"
+  zone = "us-central1-c"
+  project = "my-project-name"
+
+  share_settings {
+    share_type = "LOCAL"
+  }
+
+  specific_reservation {
+    count = 1
+    instance_properties {
+      machine_type     = "n2-standard-2"
+    }
+  }
+}
+
+# [END compute_reservation_create_local_reservation]
