07ad154a5746f8271c7c82734a21b6b5a3879ef0
The Magician <magic-modules@google.com>
2022-07-12 11:49:02 -0700

    Add region tags to no-auth Cloud Run example (#6246) (#98)
    * Add region tags to no-auth Cloud Run example
    
    * Add separate tags for IAM-policy-specific stuff
    
    * Fix region tag names
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ cloud_run_service_noauth/main.tf
@@ -0,0 +1,35 @@
+# Example of how to deploy a publicly-accessible Cloud Run application
+
+# [START cloudrun_service_noauth]
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+  }
+}
+# [END cloudrun_service_noauth]
+
+# [START cloudrun_service_noauth_iam]
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+# [END cloudrun_service_noauth_iam]
