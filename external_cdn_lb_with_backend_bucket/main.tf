7ef80bf8884ad4cb6a122f95f96f9ceed549aca4
The Magician <magic-modules@google.com>
2022-07-21 11:15:42 -0700

    Add new terraform sample - CDN XLB with Backend Bucket (#6252) (#106)
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- external_cdn_lb_with_backend_bucket/main.tf
+++ external_cdn_lb_with_backend_bucket/main.tf
@@ -1,53 +1,62 @@
 # CDN load balancer with Cloud bucket as backend
 
 # [START cloudloadbalancing_cdn_with_backend_bucket]
+
+
+# [START cloudloadbalancing_cdn_with_backend_bucket_vpc]
 # VPC
 resource "google_compute_network" "default" {
   name                    = "cdn-network"
-  provider                = google-beta
   auto_create_subnetworks = false
 }
+# [END cloudloadbalancing_cdn_with_backend_bucket_vpc]
 
+# [START cloudloadbalancing_cdn_with_backend_bucket_subnet]
 # backend subnet
 resource "google_compute_subnetwork" "default" {
   name          = "cdn-subnet"
-  provider      = google-beta
   ip_cidr_range = "10.0.1.0/24"
   region        = "us-central1"
   network       = google_compute_network.default.id
 }
+# [END cloudloadbalancing_cdn_with_backend_bucket_subnet]
 
+# [START cloudloadbalancing_cdn_with_backend_bucket_ip_address]
 # reserve IP address
 resource "google_compute_global_address" "default" {
-  provider = google-beta
   name     = "cdn-static-ip"
 }
+# [END cloudloadbalancing_cdn_with_backend_bucket_ip_address]
 
+# [START cloudloadbalancing_cdn_with_backend_bucket_forwarding_rule]
 # forwarding rule
 resource "google_compute_global_forwarding_rule" "default" {
   name                  = "cdn-forwarding-rule"
-  provider              = google-beta
   ip_protocol           = "TCP"
   load_balancing_scheme = "EXTERNAL"
   port_range            = "80"
   target                = google_compute_target_http_proxy.default.id
   ip_address            = google_compute_global_address.default.id
 }
+# [END cloudloadbalancing_cdn_with_backend_bucket_forwarding_rule]
 
+# [START cloudloadbalancing_cdn_with_backend_bucket_http_proxy]
 # http proxy
 resource "google_compute_target_http_proxy" "default" {
   name     = "cdn-target-http-proxy"
-  provider = google-beta
   url_map  = google_compute_url_map.default.id
 }
+# [END cloudloadbalancing_cdn_with_backend_bucket_http_proxy]
 
+# [START cloudloadbalancing_cdn_with_backend_bucket_url_map]
 # url map
 resource "google_compute_url_map" "default" {
   name            = "cdn-url-map"
-  provider        = google-beta
   default_service = google_compute_backend_bucket.default.id
 }
+# [END cloudloadbalancing_cdn_with_backend_bucket_url_map]
 
+# [START cloudloadbalancing_cdn_with_backend_bucket_backend_bucket]
 # backend bucket with CDN policy with default ttl settings
 resource "google_compute_backend_bucket" "default" {
   name        = "image-backend-bucket"
@@ -63,8 +72,10 @@ resource "google_compute_backend_bucket" "default" {
     serve_while_stale = 86400
   }
 }
+# [END cloudloadbalancing_cdn_with_backend_bucket_backend_bucket]
 
-# cdn backend bucket
+# [START cloudloadbalancing_cdn_with_backend_bucket_cloud_storage_bucket]
+# Cloud Storage bucket
 resource "google_storage_bucket" "default" {
   name                        = "cdn-backend-storage-bucket"
   location                    = "US"
@@ -77,61 +88,55 @@ resource "google_storage_bucket" "default" {
     not_found_page   = "404.html"
   }
 }
+# [END cloudloadbalancing_cdn_with_backend_bucket_cloud_storage_bucket]
 
+# [START cloudloadbalancing_cdn_with_backend_bucket_make_public]
 # make bucket public
 resource "google_storage_bucket_iam_member" "default" {
   bucket = google_storage_bucket.default.name
   role   = "roles/storage.objectViewer"
   member = "allUsers"
 }
+# [END cloudloadbalancing_cdn_with_backend_bucket_make_public]
 
+# [START cloudloadbalancing_cdn_with_backend_bucket_index_page]
 resource "google_storage_bucket_object" "index_page" {
-  name       = "index.html"
-  source     = "index.html"
+  name       = "index-page"
   bucket     = google_storage_bucket.default.name
-  depends_on = [local_file.index_page]
-}
-
-resource "google_storage_bucket_object" "error_page" {
-  name       = "404.html"
-  source     = "404.html"
-  bucket     = google_storage_bucket.default.name
-  depends_on = [local_file.error_page]
-}
-
-# image object for testing, try to access http://<your_lb_ip_address>/test.jpg
-resource "google_storage_bucket_object" "test_image" {
-  name         = "test.jpg"
-  source       = "test.jpg"
-  content_type = "image/jpeg"
-  bucket       = google_storage_bucket.default.name
-  depends_on   = [null_resource.test_image]
-}
-
-# cdn sample index page
-resource "local_file" "index_page" {
-  filename = "index.html"
-  content  = <<-EOT
+  content = <<-EOT
     <html><body>
     <h1>Congratulations on setting up Google Cloud CDN with Storage backend!</h1>
     </body></html>
   EOT
 }
+# [END cloudloadbalancing_cdn_with_backend_bucket_index_page]
 
-# cdn default error page
-resource "local_file" "error_page" {
-  filename = "404.html"
+# [START cloudloadbalancing_cdn_with_backend_bucket_error_page]
+resource "google_storage_bucket_object" "error_page" {
+  name       = "404-page"
+  bucket     = google_storage_bucket.default.name
   content  = <<-EOT
     <html><body>
     <h1>404 Error: Object you are looking for is no longer available!</h1>
     </body></html>
   EOT
 }
+# [END cloudloadbalancing_cdn_with_backend_bucket_error_page]
 
-# cdn sample image
-resource "null_resource" "test_image" {
-  provisioner "local-exec" {
-    command = "wget -O test.jpg  https://upload.wikimedia.org/wikipedia/commons/c/c8/Thank_you_001.jpg"
-  }
+# [START cloudloadbalancing_cdn_with_backend_bucket_image]
+# image object for testing, try to access http://<your_lb_ip_address>/test.jpg
+resource "google_storage_bucket_object" "test_image" {
+  name         = "test-object"
+# Uncomment and add valid path to an object.
+#  source       = "/path/to/an/object"
+#  content_type = "image/jpeg"
+
+# Delete after uncommenting above source and content_type attributes
+  content      = "Data as string to be uploaded"
+  content_type = "text/plain"
+
+  bucket       = google_storage_bucket.default.name
 }
+# [END cloudloadbalancing_cdn_with_backend_bucket_image]
+
 # [END cloudloadbalancing_cdn_with_backend_bucket]
