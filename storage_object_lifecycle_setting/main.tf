ad05e2b35e084a578724896aeec469be2b454733
The Magician <magic-modules@google.com>
2022-08-25 11:57:11 -0700

    fix beta tests (#6452) (#140)
    Signed-off-by: Modular Magician <magic-modules@google.com>
    
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- storage_object_lifecycle_setting/main.tf
+++ storage_object_lifecycle_setting/main.tf
@@ -1,5 +1,6 @@
 # [START storage_create_lifecycle_setting_tf]
 resource "google_storage_bucket" "auto_expire" {
+  provider      = google-beta
   name          = "example-bucket"
   location      = "US"
   uniform_bucket_level_access = true
