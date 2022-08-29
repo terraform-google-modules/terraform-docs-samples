7ccc2d6b4099aacb805cb8469144c320aa8cfeec
bharathkkb <bharathkrishnakb@gmail.com>
2022-08-29 22:24:54 +0000

    cloud run sample update

--- cloud_run_service_scheduled/main.tf
+++ cloud_run_service_scheduled/main.tf
@@ -1,31 +1,25 @@
 resource "google_project_service" "run_api" {
-  project                    = "my-project-name"
   service                    = "run.googleapis.com"
-  disable_dependent_services = true
   disable_on_destroy         = false
 }
 
 resource "google_project_service" "iam_api" {
-  project                    = "my-project-name"
   service                    = "iam.googleapis.com"
   disable_on_destroy         = false
 }
 
 resource "google_project_service" "resource_manager_api" {
-  project                    = "my-project-name"
   service                    = "cloudresourcemanager.googleapis.com"
   disable_on_destroy         = false
 }
 
 resource "google_project_service" "scheduler_api" {
-  project                    = "my-project-name"
   service                    = "cloudscheduler.googleapis.com"
   disable_on_destroy         = false
 }
 
 # [START cloudrun_service_scheduled_service]
 resource "google_cloud_run_service" "default" {
-  project  = "my-project-name"
   name     = "my-scheduled-service"
   location = "us-central1"
 
@@ -51,7 +45,6 @@ resource "google_cloud_run_service" "default" {
 
 # [START cloudrun_service_scheduled_sa]
 resource "google_service_account" "default" {
-  project      = "my-project-name"
   account_id   = "scheduler-sa"
   description  = "Cloud Scheduler service account; used to trigger scheduled Cloud Run jobs."
   display_name = "scheduler-sa"
@@ -93,7 +86,6 @@ resource "google_cloud_scheduler_job" "default" {
 
 # [START cloudrun_service_scheduled_iam]
 resource "google_cloud_run_service_iam_member" "default" {
-  project = "my-project-name"
   location = google_cloud_run_service.default.location
   service = google_cloud_run_service.default.name
   role = "roles/run.invoker"
