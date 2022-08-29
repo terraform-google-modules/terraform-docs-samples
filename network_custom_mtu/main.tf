2998acd0bb382640efea62fb16f1db807cacadae
bharathkkb <bharathkrishnakb@gmail.com>
2022-08-29 22:28:03 +0000

    misc network sample updates

--- network_custom_mtu/main.tf
+++ network_custom_mtu/main.tf
@@ -1,6 +1,5 @@
 # [START vpc_auto_create]
 resource "google_compute_network" "vpc_network" {
-  project                 = "my-project-name"
   name                    = "vpc-network"
   auto_create_subnetworks = true
   mtu                     = 1460
