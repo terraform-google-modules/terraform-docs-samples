e5e5f4def89421809dfb9b02bf22a4ac876968ed
Eric Dong <itseric@google.com>
2022-09-07 09:28:12 -0400

    Changed the description

--- vertex_ai_metadata_store/main.tf
+++ vertex_ai_metadata_store/main.tf
@@ -2,7 +2,7 @@
 resource "google_vertex_ai_metadata_store" "default" {
   name          = "test-store"
   provider      = google-beta
-  description   = "Store to test the terraform module"
+  description   = "Example metadata store"
   region        = "us-central1"
 }
 # [END vertex_ai_metadata_store]
