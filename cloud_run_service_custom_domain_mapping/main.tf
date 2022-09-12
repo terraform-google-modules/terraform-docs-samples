2b20658815c4fb476211b7f5f54afd7c224316a0
Roger Martinez <rogerthatdev@gmail.com>
2022-09-12 11:17:34 -0700

    sample for cloud run custom domain mapping

---
+++ cloud_run_service_custom_domain_mapping/main.tf
@@ -0,0 +1,31 @@
+# [START cloudrun_custom_domain_mapping_run_service]
+data "google_project" "project" {}
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloud-run-srv"
+  location = "us-central1"
+  template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun/hello"
+      }
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+# [END cloudrun_custom_domain_mapping_run_service]
+# [START cloudrun_custom_domain_mapping]
+resource "google_cloud_run_domain_mapping" "default" {
+  name     = "verified-domain.com"
+  location = google_cloud_run_service.default.location
+  metadata {
+    namespace = data.google_project.project.project_id
+  }
+  spec {
+    route_name = google_cloud_run_service.default.name
+  }
+}
+# [END cloudrun_custom_domain_mapping]
