efe3905ca91767231431b9bb26f7a8f75f1dbebf
2tricpony <105741076+2tricpony@users.noreply.github.com>
2022-09-13 14:47:37 -0700

    Adding doc samples

--- sql_postgres_instance_ssm/main.tf
+++ sql_postgres_instance_ssm/main.tf
@@ -1,8 +1,9 @@
-# [START cloud_sql_mysql_instance_ssm]
+# [START cloud_sql_postgres_instance_ssm]
 resource "google_sql_database_instance" "postgres_ssm_instance_name" {
-  name             = "mysql-ssm-instance-name"
-  region           = "asia-northeast1"
-  database_version = "POSTGRES_12"
+  name                = "postgres-ssm-instance-name"
+  region              = "asia-northeast1"
+  database_version    = "POSTGRES_14"
+  maintenance_version = "POSTGRES_14_4.R20220710.01_07"
   settings {
     tier              = "db-f1-micro"
   }
