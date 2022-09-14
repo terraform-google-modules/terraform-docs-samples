f372a36457472b160b04d1cc527dc266b770b53f
Sampath M <sampathm@google.com>
2022-09-14 20:26:39 +0200

    Add dev-rel tags resource wise.

--- shared_vpc_http_ilb_with_mig_backend/main.tf
+++ shared_vpc_http_ilb_with_mig_backend/main.tf
@@ -1,13 +1,16 @@
 # Shared VPC Internal HTTP load balancer with a managed instance group backend
 
 # [START cloudloadbalancing_shared_vpc_http_ilb_example]
+# [START cloudloadbalancing_shared_vpc_http_ilb_network]
 # VPC network
 resource "google_compute_network" "default" {
   name                    = "l7-ilb-network"
   auto_create_subnetworks = false
   project                 = "my-host-project"
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_network]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_proxy_subnet]
 # proxy-only subnet
 resource "google_compute_subnetwork" "proxy_subnet" {
   name          = "l7-ilb-proxy-subnet"
@@ -18,7 +21,9 @@ resource "google_compute_subnetwork" "proxy_subnet" {
   network       = google_compute_network.default.id
   project       = "my-host-project"
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_proxy_subnet]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_ilb_subnet]
 # backend subnet
 resource "google_compute_subnetwork" "ilb_subnet" {
   name          = "l7-ilb-subnet"
@@ -27,7 +32,9 @@ resource "google_compute_subnetwork" "ilb_subnet" {
   network       = google_compute_network.default.id
   project       = "my-host-project"
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_ilb_subnet]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_fw_iap]
 # allow all access from IAP and health check ranges
 resource "google_compute_firewall" "fw_iap" {
   project       = "my-host-project"
@@ -39,7 +46,9 @@ resource "google_compute_firewall" "fw_iap" {
     protocol = "tcp"
   }
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_fw_iap]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_fw_ilb_to_backends]
 # allow http from proxy subnet to backends
 resource "google_compute_firewall" "fw_ilb_to_backends" {
   project       = "my-host-project"
@@ -53,7 +62,9 @@ resource "google_compute_firewall" "fw_ilb_to_backends" {
     ports    = ["80", "443", "8080"]
   }
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_fw_ilb_to_backends]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_forwarding_rule]
 # forwarding rule
 resource "google_compute_forwarding_rule" "default" {
   name                  = "l7-ilb-forwarding-rule"
@@ -68,7 +79,9 @@ resource "google_compute_forwarding_rule" "default" {
   project               = "my-service-project-01"
   depends_on            = [google_compute_subnetwork.proxy_subnet]
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_forwarding_rule]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_regional_http_proxy]
 # HTTP target proxy
 resource "google_compute_region_target_http_proxy" "default" {
   name    = "l7-ilb-target-http-proxy"
@@ -76,7 +89,9 @@ resource "google_compute_region_target_http_proxy" "default" {
   url_map = google_compute_region_url_map.default.id
   project = "my-service-project-01"
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_regional_http_proxy]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_url_map]
 # URL map
 resource "google_compute_region_url_map" "default" {
   name            = "l7-ilb-regional-url-map"
@@ -84,7 +99,9 @@ resource "google_compute_region_url_map" "default" {
   default_service = google_compute_region_backend_service.default.id
   project         = "my-service-project-01"
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_url_map]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_regional_hc]
 # regional health check
 resource "google_compute_region_health_check" "default" {
   project = "my-service-project-02"
@@ -94,7 +111,9 @@ resource "google_compute_region_health_check" "default" {
     port_specification = "USE_SERVING_PORT"
   }
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_regional_hc]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_regional_backend]
 # regional backend service
 resource "google_compute_region_backend_service" "default" {
   project               = "my-service-project-02"
@@ -110,7 +129,9 @@ resource "google_compute_region_backend_service" "default" {
     capacity_scaler = 1.0
   }
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_regional_backend]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_hc]
 # health check
 resource "google_compute_health_check" "default" {
   project            = "my-service-project-02"
@@ -121,7 +142,9 @@ resource "google_compute_health_check" "default" {
     port = "80"
   }
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_hc]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_instance_template]
 # instance template
 resource "google_compute_instance_template" "default" {
   project      = "my-service-project-02"
@@ -166,12 +189,14 @@ resource "google_compute_instance_template" "default" {
     EOF1
   }
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_instance_template]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_mig]
 # MIG
 resource "google_compute_region_instance_group_manager" "default" {
-  project    = "my-service-project-02"
-  name       = "l7-ilb-mig1"
-  region     = "us-central1"
+  project = "my-service-project-02"
+  name    = "l7-ilb-mig1"
+  region  = "us-central1"
   version {
     instance_template = google_compute_instance_template.default.id
     name              = "primary"
@@ -184,7 +209,9 @@ resource "google_compute_region_instance_group_manager" "default" {
   }
   depends_on = [google_project_iam_binding.default]
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_mig]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_iam_binding]
 data "google_project" "service_project02" {
   project_id = "my-service-project-02"
 }
@@ -198,7 +225,9 @@ resource "google_project_iam_binding" "default" {
     "serviceAccount:${data.google_project.service_project02.number}@cloudservices.gserviceaccount.com",
   ]
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_iam_binding]
 
+# [START cloudloadbalancing_shared_vpc_http_ilb_test_instance]
 # test instance
 resource "google_compute_instance" "test_vm" {
   project      = "my-service-project-02"
@@ -221,4 +250,5 @@ resource "google_compute_instance" "test_vm" {
   }
   depends_on = [google_project_iam_binding.default]
 }
+# [END cloudloadbalancing_shared_vpc_http_ilb_test_instance]
 # [END cloudloadbalancing_shared_vpc_http_ilb_example]
