f1b64430a1ed3f46e48ef15218b57af76ca60954
2tricpony <105741076+2tricpony@users.noreply.github.com>
2022-09-19 17:32:24 -0700

    Doc samples for Self Service Maintenance feature for Cloud SQL (#160)
    * Create main.tf
    
    * Adding doc samples
    
    * Removing new line
    
    * Update sql_sqlserver_instance_ssm/test.yaml
    
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>
    
    * Update sql_mysql_instance_ssm/test.yaml
    
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>
    
    * Update sql_postgres_instance_ssm/test.yaml
    
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>
    
    * Adding new line
    
    Co-authored-by: Bharath KKB <bharathkrishnakb@gmail.com>

---
+++ sql_mysql_instance_ssm/main.tf
@@ -0,0 +1,12 @@
+# [START cloud_sql_mysql_instance_ssm]
+resource "google_sql_database_instance" "mysql_ssm_instance_name" {
+  name                = "mysql-ssm-instance-name"
+  region              = "asia-northeast1"
+  database_version    = "MYSQL_5_7"
+  maintenance_version = "MYSQL_5_7_38.R20220809.02_00"
+  settings {
+    tier              = "db-f1-micro"
+  }
+  deletion_protection = false # set to true to prevent destruction of the resource
+}
+# [END cloud_sql_mysql_instance_ssm]
