ab78fcb80226bfde01074d944a5b136f87fb63b5
Betsy Lichtenberg <betsyl@google.com>
2022-09-29 09:14:54 -0700

    Reverted unnecessary changes to file

--- cloudfunctions2_basic/main.tf
+++ cloudfunctions2_basic/main.tf
@@ -1,19 +1,4 @@
 # [START functions_v2_basic]
-data "google_project" "project" {
-  provider = google-beta
-}
-
-resource "google_service_account" "account" {
-  account_id = "gcf-sa"
-  display_name = "Test Service Account"
-}
-
-resource "google_project_iam_member" "bucket" { 
-  project = data.google_project.project.project_id
-  role    = "roles/storage.objectViewer"
-  member  = "serviceAccount:${google_service_account.account.email}"
-}
-
 resource "random_id" "bucket_prefix" {
   byte_length = 8
 }
