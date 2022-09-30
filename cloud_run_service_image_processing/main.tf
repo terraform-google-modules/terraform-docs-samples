2186927f97936f8e7532e6a97dabe01ded16f94f
rogerthatdev <roger.that.dev@gmail.com>
2022-09-30 14:39:16 -0700

    add prereq resources from existing pubsub sample

--- cloud_run_service_image_processing/main.tf
+++ cloud_run_service_image_processing/main.tf
@@ -1,3 +1,50 @@
+# [START cloudrun_service_existing_pubsub]
+resource "google_service_account" "sa" {
+  account_id   = "cloud-run-pubsub-invoker"
+  display_name = "Cloud Run Pub/Sub Invoker"
+}
+
+resource "google_cloud_run_service_iam_binding" "binding" {
+  location = google_cloud_run_service.default.location
+  service  = google_cloud_run_service.default.name
+  role     = "roles/run.invoker"
+  members  = ["serviceAccount:${google_service_account.sa.email}"]
+}
+
+data "google_project" "project" {
+}
+
+resource "google_project_service_identity" "pubsub_agent" {
+  provider = google-beta
+  project  = data.google_project.project.project_id
+  service  = "pubsub.googleapis.com"
+}
+
+resource "google_project_iam_binding" "project_token_creator" {
+  project  = data.google_project.project.project_id
+  role    = "roles/iam.serviceAccountTokenCreator"
+  members = ["serviceAccount:${google_project_service_identity.pubsub_agent.email}"]
+}
+
+resource "google_pubsub_topic" "topic" {
+  name = "pubsub_topic"
+}
+
+resource "google_pubsub_subscription" "subscription" {
+  name  = "pubsub_subscription"
+  topic = google_pubsub_topic.topic.name
+  push_config {
+    push_endpoint = google_cloud_run_service.default.status[0].url
+    oidc_token {
+      service_account_email = google_service_account.sa.email
+    }
+    attributes = {
+      x-goog-version = "v1"
+    }
+  }
+}
+# [END cloudrun_service_existing_pubsub]
+
 # [START cloudrun_service_image_processing_buckets]
 resource "random_id" "bucket_suffix" {
   byte_length = 8
@@ -40,7 +87,7 @@ resource "google_cloud_run_service" "default" {
 data "google_storage_project_service_account" "gcs_account" {}
 
 resource "google_pubsub_topic_iam_binding" "binding" {
-  topic   = google_pubsub_topic.imageproc.name
+  topic   = google_pubsub_topic.topic.name
   role    = "roles/pubsub.publisher"
   members = ["serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"]
 }
@@ -49,7 +96,7 @@ resource "google_storage_notification" "notification" {
   provider       = google
   bucket         = google_storage_bucket.imageproc_input.name
   payload_format = "JSON_API_V1"
-  topic          = google_pubsub_topic.imageproc.id
+  topic          = google_pubsub_topic.topic.id
   depends_on     = [google_pubsub_topic_iam_binding.binding]
 }
 # [END cloudrun_service_image_processing_notifications]
