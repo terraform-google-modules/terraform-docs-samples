e1fbe8075724d186118bba7cb96b38494d2bdef1
Betsy Lichtenberg <betsyl@google.com>
2022-10-04 12:36:05 -0700

    Fixed copy paste error

--- flask_google_cloud_quickstart/main.tf
+++ flask_google_cloud_quickstart/main.tf
@@ -51,7 +51,7 @@ resource "google_compute_firewall" "ssh" {
     protocol = "tcp"
   }
   direction     = "INGRESS"
-  network       = google_compute_firewall
+  network       = google_compute_network.vpc_network.id
   priority      = 1000
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["ssh"]
