041af8bb4d878beb55fc3b90f31abe3394ae898f
Betsy Lichtenberg <betsyl@google.com>
2022-10-04 13:34:22 -0700

    Fixed python version in startup script (#188)

--- flask_google_cloud_quickstart/main.tf
+++ flask_google_cloud_quickstart/main.tf
@@ -31,7 +31,7 @@ resource "google_compute_instance" "default" {
   }
 
   # Install Flask
-  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
 
   network_interface {
     subnetwork = google_compute_subnetwork.default.id
