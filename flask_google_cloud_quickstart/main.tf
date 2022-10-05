efc3e1a86b8209f488a5a98bb27e3b52b0a61e56
Betsy Lichtenberg <betsyl@google.com>
2022-10-05 10:12:45 -0700

    Removed unnecessary metadata (#189)

--- flask_google_cloud_quickstart/main.tf
+++ flask_google_cloud_quickstart/main.tf
@@ -21,9 +21,6 @@ resource "google_compute_instance" "default" {
   zone         = "us-west1-a"
   tags         = ["ssh"]
 
-  metadata = {
-    enable-oslogin = "TRUE"
-  }
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
