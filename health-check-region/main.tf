e706418e46ae7414440573daa800973c51ff418a
Sahana C Prabhakar <sahanacp@google.com>
2022-10-28 09:41:50 +0530

    Prepared health-check-global for inclusion in C.G.C. documentation

--- health-check-region/main.tf
+++ health-check-region/main.tf
@@ -15,19 +15,6 @@
  */
 
 # [START cloudloadbalancing_regional_health_check]
-resource "google_compute_region_health_check" "tcp-health-check-region-west" {
-  name     = "tcp-health-check-region-west"
-
-  timeout_sec        = 1
-  check_interval_sec = 1
-
-  tcp_health_check {
-    port = "80"
-  }
-  region = "us-west1"
-}
-
-# [START cloudloadbalancing_regional_health_check]
 resource "google_compute_region_health_check" "default" {
   name               = "tcp-health-check-region-west"
   timeout_sec        = 5
