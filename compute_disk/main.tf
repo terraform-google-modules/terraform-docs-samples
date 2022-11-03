c0fb1c6a3572354c65cad31f50d42d98644542c9
Betsy Lichtenberg <betsyl@google.com>
2022-11-03 13:52:53 -0700

    Added parent region tag to group two resources together

--- compute_disk/main.tf
+++ compute_disk/main.tf
@@ -28,6 +28,8 @@ resource "google_compute_disk" "default" {
 }
 # [END compute_disk_clone_single_zone]
 
+# [START compute_disk_clone_regional]
+
 # [START compute_disk_snapshot_zonal]
 resource "google_compute_snapshot" "snapdisk" {
   name        = "snapshot-name"
@@ -36,7 +38,6 @@ resource "google_compute_snapshot" "snapdisk" {
 }
 # [END compute_disk_snapshot_zonal]
 
-# [START compute_disk_clone_regional]
 resource "google_compute_region_disk" "regiondisk" {
   name                      = "region-disk-name"
   snapshot                  = google_compute_snapshot.snapdisk.id
