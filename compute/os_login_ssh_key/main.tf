9a8db2a67cb331cb1e30a2b7d844ffded55eebfa
Betsy Lichtenberg <betsyl@google.com>
2022-11-16 09:37:00 -0800

    Added license and region tag

--- compute/os_login_ssh_key/main.tf
+++ compute/os_login_ssh_key/main.tf
@@ -1,3 +1,20 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# [START compute_os_login_ssh_public_key]
 data "google_client_openid_userinfo" "me" {
 }
 
@@ -5,3 +22,4 @@ resource "google_os_login_ssh_public_key" "default" {
   user = data.google_client_openid_userinfo.me.email
   key  = file("sample-private.key") # path/to/ssl/id_rsa.pub
 }
+# [END compute_os_login_ssh_public_key]
