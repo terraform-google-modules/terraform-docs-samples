4616adf41bb6d5c964c4c663b9758784885fafc3
Betsy Lichtenberg <betsyl@google.com>
2022-11-16 13:09:58 -0800

    Fixed file name

--- compute/os_login_ssh_key/main.tf
+++ compute/os_login_ssh_key/main.tf
@@ -20,6 +20,6 @@ data "google_client_openid_userinfo" "me" {
 
 resource "google_os_login_ssh_public_key" "default" {
   user = data.google_client_openid_userinfo.me.email
-  key  = file("sample-private.key") # path/to/ssl/id_rsa.pub
+  key  = file("id_rsa.pub") # path/to/ssl/id_rsa.pub
 }
 # [END compute_os_login_ssh_public_key]
