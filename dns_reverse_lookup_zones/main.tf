bcc3a808baffb7b2ffecee883ee041941e4bef4a
Sampath M <sampathm@google.com>
2022-12-13 00:07:36 +0100

    chore(dns): example cloud dns reverse lookup

---
+++ dns_reverse_lookup_zones/main.tf
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# Google Cloud Documentation https://cloud.google.com/dns/docs/zones/managed-reverse-lookup-zones#console
+
+# [START dns_managed_zone_reverse_lookup]
+resource "google_dns_managed_zone" "reverse_lookup_zone" {
+  name           = "my-new-zone"
+  description    = "Example DNS reverse lookup zone"
+  provider       = google-beta
+  visibility     = "private"
+  dns_name       = "2.2.20.20.in-addr.arpa."
+  reverse_lookup = "true"
+}
+# [END dns_managed_zone_reverse_lookup]
