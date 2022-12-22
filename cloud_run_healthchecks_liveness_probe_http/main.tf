edbb36c99893bd13c4e3cd01b6fad8e8c9577adb
Patti Shin <pattishin@users.noreply.github.com>
2022-12-22 15:43:00 -0800

    feat: add in cloud_run_healthchecks_liveness_probe_http sample

---
+++ cloud_run_healthchecks_liveness_probe_http/main.tf
@@ -0,0 +1,50 @@
+provider "google-beta" {
+  project = "tf-cloud-run-healthcheck"
+  region  = "us-central1"
+}
+
+data "google_project" "project" {
+  project_id = "tf-cloud-run-healthcheck"
+}
+
+# Enable Cloud Run API
+resource "google_project_service" "cloudrun_api" {
+  service            = "run.googleapis.com"
+  disable_on_destroy = false
+  project = "tf-cloud-run-healthcheck"
+}
+
+# Create Cloud Run Container with HTTP liveness probe
+#[START cloud_run_healthchecks_liveness_probe_http]
+resource "google_cloud_run_service" "default" {
+  provider = google-beta
+  name     = "cloudrun-service-healthcheck"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+        liveness_probe {
+          failure_threshold = 5
+          initial_delay_seconds = 10
+          timeout_seconds = 3
+          period_seconds = 3
+          http_get {
+            path = "/"
+            http_headers {
+              name = "Access-Control-Allow-Origin"
+              value = "*"
+            }
+          }
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+#[END cloud_run_healthchecks_liveness_probe_http]
