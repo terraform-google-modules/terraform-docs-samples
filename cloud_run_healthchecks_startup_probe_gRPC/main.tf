bad287525345f6a6b19812d990ab60d202bb2c0f
Patti Shin <pattishin@users.noreply.github.com>
2022-12-22 15:38:30 -0800

    feat: add in cloud_run_healthchecks_startup_probe_gRPC sample

---
+++ cloud_run_healthchecks_startup_probe_gRPC/main.tf
@@ -0,0 +1,47 @@
+provider "google-beta" {
+  project = "your-project-id"
+  region  = "us-central1"
+}
+
+data "google_project" "project" {
+  project_id = "your-project-id"
+}
+
+# Enable Cloud Run API
+resource "google_project_service" "cloudrun_api" {
+  service            = "run.googleapis.com"
+  disable_on_destroy = false
+  project = "your-project-id"
+}
+
+# Create Cloud Run Container with pre-existing gRPC service
+#[START cloud_run_healthchecks_startup_probe_gRPC]
+resource "google_cloud_run_service" "default" {
+  provider = google-beta
+  name     = "cloudrun-service-healthcheck"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+        startup_probe {
+          failure_threshold = 5
+          initial_delay_seconds = 10
+          timeout_seconds = 3
+          period_seconds = 3
+          grpc {
+            port = 8080
+            service = "grpc.health.v1.Health" # gRPC service should already exist
+          }
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+#[END cloud_run_healthchecks_startup_probe_gRPC]
