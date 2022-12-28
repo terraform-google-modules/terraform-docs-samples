a3bcaca669ab1ac8223cc2a2b29b773bab505df4
Betsy Lichtenberg <betsyl@google.com>
2022-12-27 16:32:40 -0800

    Move region tag

--- compute_disk/main.tf
+++ compute_disk/main.tf
@@ -28,8 +28,6 @@ resource "google_compute_disk" "default" {
 }
 # [END compute_disk_clone_single_zone]
 
-# [START compute_disk_clone_regional]
-
 # [START compute_disk_snapshot_zonal]
 resource "google_compute_snapshot" "snapdisk" {
   name        = "snapshot-name"
@@ -38,6 +36,7 @@ resource "google_compute_snapshot" "snapdisk" {
 }
 # [END compute_disk_snapshot_zonal]
 
+# [START compute_disk_clone_regional]
 resource "google_compute_region_disk" "regiondisk" {
   name                      = "region-disk-name"
   snapshot                  = google_compute_snapshot.snapdisk.id
