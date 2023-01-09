1fdeeddfa9f4b3a1913640106ddc9790b8831f73
Betsy Lichtenberg <betsyl@google.com>
2023-01-09 10:22:29 -0800

    Deleted provider beta and added ssh-allow to test VM

--- internal_tcp_udp_lb_with_mig_backend/main.tf
+++ internal_tcp_udp_lb_with_mig_backend/main.tf
@@ -20,14 +20,12 @@
 # VPC
 resource "google_compute_network" "ilb_network" {
   name                    = "l4-ilb-network"
-  provider                = google-beta
   auto_create_subnetworks = false
 }
 
 # backend subnet
 resource "google_compute_subnetwork" "ilb_subnet" {
   name          = "l4-ilb-subnet"
-  provider      = google-beta
   ip_cidr_range = "10.0.1.0/24"
   region        = "europe-west1"
   network       = google_compute_network.ilb_network.id
@@ -37,7 +35,6 @@ resource "google_compute_subnetwork" "ilb_subnet" {
 resource "google_compute_forwarding_rule" "google_compute_forwarding_rule" {
   name                  = "l4-ilb-forwarding-rule"
   backend_service       = google_compute_region_backend_service.default.id
-  provider              = google-beta
   region                = "europe-west1"
   ip_protocol           = "TCP"
   load_balancing_scheme = "INTERNAL"
@@ -50,7 +47,6 @@ resource "google_compute_forwarding_rule" "google_compute_forwarding_rule" {
 # backend service
 resource "google_compute_region_backend_service" "default" {
   name                  = "l4-ilb-backend-subnet"
-  provider              = google-beta
   region                = "europe-west1"
   protocol              = "TCP"
   load_balancing_scheme = "INTERNAL"
@@ -64,7 +60,6 @@ resource "google_compute_region_backend_service" "default" {
 # instance template
 resource "google_compute_instance_template" "instance_template" {
   name         = "l4-ilb-mig-template"
-  provider     = google-beta
   machine_type = "e2-small"
   tags         = ["allow-ssh", "allow-health-check"]
 
@@ -112,7 +107,6 @@ resource "google_compute_instance_template" "instance_template" {
 # health check
 resource "google_compute_region_health_check" "default" {
   name     = "l4-ilb-hc"
-  provider = google-beta
   region   = "europe-west1"
   http_health_check {
     port = "80"
@@ -122,7 +116,6 @@ resource "google_compute_region_health_check" "default" {
 # MIG
 resource "google_compute_region_instance_group_manager" "mig" {
   name     = "l4-ilb-mig1"
-  provider = google-beta
   region   = "europe-west1"
   version {
     instance_template = google_compute_instance_template.instance_template.id
@@ -135,7 +128,6 @@ resource "google_compute_region_instance_group_manager" "mig" {
 # allow all access from health check ranges
 resource "google_compute_firewall" "fw_hc" {
   name          = "l4-ilb-fw-allow-hc"
-  provider      = google-beta
   direction     = "INGRESS"
   network       = google_compute_network.ilb_network.id
   source_ranges = ["130.211.0.0/22", "35.191.0.0/16", "35.235.240.0/20"]
@@ -148,7 +140,6 @@ resource "google_compute_firewall" "fw_hc" {
 # allow communication within the subnet
 resource "google_compute_firewall" "fw_ilb_to_backends" {
   name          = "l4-ilb-fw-allow-ilb-to-backends"
-  provider      = google-beta
   direction     = "INGRESS"
   network       = google_compute_network.ilb_network.id
   source_ranges = ["10.0.1.0/24"]
@@ -166,7 +157,6 @@ resource "google_compute_firewall" "fw_ilb_to_backends" {
 # allow SSH
 resource "google_compute_firewall" "fw_ilb_ssh" {
   name      = "l4-ilb-fw-ssh"
-  provider  = google-beta
   direction = "INGRESS"
   network   = google_compute_network.ilb_network.id
   allow {
@@ -180,7 +170,7 @@ resource "google_compute_firewall" "fw_ilb_ssh" {
 # test instance
 resource "google_compute_instance" "vm_test" {
   name         = "l4-ilb-test-vm"
-  provider     = google-beta
+  tags         = ["allow-ssh"]
   zone         = "europe-west1-b"
   machine_type = "e2-small"
   network_interface {
