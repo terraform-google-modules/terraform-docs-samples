e3438fe0e6d3377b86e0f09038d2d4baca2a2a24
rogerthatdev <roger.that.dev@gmail.com>
2023-01-20 17:06:57 -0800

    remove newline from csv destination var

--- migrate-csv.sh
+++ migrate-csv.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-# source=${1%/}
-# destination=${2%/}
-
-# if [ ! -d "$source" ]; then
-#     echo "Source \"${source}\" does not exist"
-#     exit 2
-# fi
 INPUT=$1
 IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 while read source destination
 do
+    destination=$(echo $destination | sed 's/\r//g')
 	echo "moving ${source} to ${destination}"
     git mv $source $destination
     git commit -m "move ${source} to ${destination}"
-
     saved=`git rev-parse HEAD`
     git reset --hard HEAD^
     git mv ${source} ${source}-copy
@@ -29,18 +22,3 @@ do
 done < $INPUT
 
 exit
-echo "Moving ${source} to ${destination}"
-
-git mv $source $destination
-git commit -m "move ${source} to ${destination}"
-
-saved=`git rev-parse HEAD`
-git reset --hard HEAD^
-git mv ${source} ${source}-copy
-git commit -m "temporary copy of ${source}"
-
-git merge $saved
-git commit -a -m "merge copy of ${source}"
-
-git mv ${source}-copy ${source}
-git commit -m "merge original copy of ${source}"
