16b3e2c9241f6e96a0a120d2c000929a77917861
Betsy Lichtenberg <betsyl@google.com>
2023-01-26 12:00:35 -0800

    Added basic VM sample

---
+++ compute/basic_vm/main.tf
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# [START compute_basic_vm_sample]
+resource "google_compute_instance" "default" {
+  name         = "my-vm"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1404-trusty-v20160627"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
+# [END compute_compute_basic_vm_sample]
