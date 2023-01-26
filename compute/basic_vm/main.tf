6aeb724f7e21c99a5aee93ede33dac56a24d1eb2
Betsy Lichtenberg <betsyl@google.com>
2023-01-26 13:07:48 -0800

    added vm in custom subnet

--- compute/basic_vm/main.tf
+++ compute/basic_vm/main.tf
@@ -14,7 +14,9 @@
  * limitations under the License.
  */
 
-# [START compute_basic_vm_sample]
+# [START compute_basic_vm_default_vpc_network]
+# Create a VM in the `default` VPC network and subnet
+
 resource "google_compute_instance" "default" {
   name         = "my-vm"
   machine_type = "n1-standard-1"
@@ -31,4 +33,40 @@ resource "google_compute_instance" "default" {
     access_config {}
   }
 }
-# [END compute_compute_basic_vm_sample]
+# [END compute_basic_vm_default_vpc_network]
+
+# [START vpc_compute_basic_vm_custom_vpc_network]
+resource "google_compute_network" "custom" {
+  name                    = "my-network"
+  auto_create_subnetworks = false
+}
+# [END vpc_compute_basic_vm_custom_vpc_network]
+
+# [START vpc_compute_basic_vm_custom_vpc_subnet]
+resource "google_compute_subnetwork" "custom" {
+  name          = "my-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "europe-west1"
+  network       = google_compute_network.custom.id
+}
+# [END vpc_compute_basic_vm_custom_vpc_subnet]
+
+# [START compute_basic_vm_custom_vpc_subnet]
+# Create a VM in a custom VPC network and subnet
+
+resource "google_compute_instance" "custom_subnet" {
+  name         = "my-vm-instance"
+  tags         = ["allow-ssh"]
+  zone         = "europe-west1-b"
+  machine_type = "e2-small"
+  network_interface {
+    network    = google_compute_network.custom.id
+    subnetwork = google_compute_subnetwork.custom.id
+  }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    } 
+  }
+} 
+# [END compute_basic_vm_custom_vpc_subnet]
