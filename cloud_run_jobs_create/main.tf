cb3f9390a50d69109d994ccad22095b3f1e37ace
Patti Shin <pattishin@users.noreply.github.com>
2023-02-03 21:43:55 -0800

    feat: initial set of samples for jobs

---
+++ cloud_run_jobs_create/main.tf
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+provider "google-beta" {
+  region = "us-central1"
+}
+
+# Project data
+data "google_project" "project" {
+  project_id = "your-project-id"
+}
+
+# Enable Cloud Run API
+resource "google_project_service" "cloudrun_api" {
+  service            = "run.googleapis.com"
+  disable_on_destroy = false
+  project            = data.google_project.project.project_id
+}
+
+# Create basic Cloud Run Job using sample container image
+#[START cloud_run_jobs_create]
+resource "google_cloud_run_v2_job" "default" {
+  provider     = google-beta
+  name         = "cloud-run-job"
+  location     = "us-central1"
+  launch_stage = "BETA"
+  project      = data.google_project.project.project_id
+
+  template {
+    template {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/job:latest"
+      }
+    }
+  }
+}
+#[END cloud_run_jobs_create]
---
+++ cloud_run_jobs_execute_jobs_on_schedule/main.tf
@@ -0,0 +1,93 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+provider "google-beta" {
+  region = "us-central1"
+}
+
+# Project data
+data "google_project" "project" {
+  project_id = "your-project-id"
+}
+
+# Compute service account
+data "google_compute_default_service_account" "default" {
+  project = data.google_project.project.project_id
+}
+
+# Enable Cloud Run API
+resource "google_project_service" "cloudrun_api" {
+  service            = "run.googleapis.com"
+  disable_on_destroy = false
+  project            = data.google_project.project.project_id
+}
+
+# Enable Compute Engine API
+resource "google_project_service" "computeengine_api" {
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+  project            = data.google_project.project.project_id
+}
+
+# Enable Cloud Scheduler API
+resource "google_project_service" "cloudscheduler_api" {
+  service            = "cloudscheduler.googleapis.com"
+  disable_on_destroy = false
+  project            = data.google_project.project.project_id
+}
+
+resource "google_cloud_run_v2_job" "default" {
+  provider     = google-beta
+  name         = "cloud-run-job"
+  location     = "us-central1"
+  launch_stage = "BETA"
+  project      = data.google_project.project.project_id
+
+  template {
+    template {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/job:latest"
+      }
+    }
+  }
+}
+
+#[START cloud_run_jobs_execute_jobs_on_schedule]
+resource "google_cloud_scheduler_job" "job" {
+  provider         = google-beta
+  name             = "schedule-job"
+  description      = "test http job"
+  schedule         = "*/8 * * * *"
+  attempt_deadline = "320s"
+  region           = "us-central1"
+  project          = data.google_project.project.project_id
+
+  retry_config {
+    retry_count = 3
+  }
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://${google_cloud_run_v2_job.default.location}-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/${data.google_project.project.number}/jobs/${google_cloud_run_v2_job.default.name}:run"
+
+    oauth_token {
+      service_account_email = data.google_compute_default_service_account.default.email
+    }
+  }
+
+  depends_on = [resource.google_project_service.cloudscheduler_api, resource.google_cloud_run_v2_job.default, resource.google_cloud_run_v2_job.default]
+}
+#[END cloud_run_jobs_execute_jobs_on_schedule]
