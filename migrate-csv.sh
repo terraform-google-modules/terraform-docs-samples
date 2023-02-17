af9692f3fc8dedb7725b2ba79a9d755fa14aeed1
Roger Martinez <31829545+rogerthatdev@users.noreply.github.com>
2023-02-16 16:51:11 -0800

    Updates to migration script (#306)
    * skip first line of csv
    
    * lose requirement for new line at end of csv
    
    * bash formatting
    
    * add a continue prompt
    
    ---------
    
    Co-authored-by: Roger Martinez <rogerthatdev@gmail.com>

--- migrate-csv.sh
+++ migrate-csv.sh
@@ -4,7 +4,7 @@
 # source-directory-1,destination-directory-1
 # source-directory-2,destination-directory-2
 #
-# Make sure that the csv fed into this script has no header, 
+# Make sure that the csv fed into this script has no header,
 # and ends with a new line
 
 # Formatting variables
@@ -15,34 +15,45 @@ ARROW='\033[0;32m\xe2\x89\xab'
 
 INPUT=$1
 IFS=','
-[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 1; }
+[ ! -f $INPUT ] && {
+    echo "$INPUT file not found"
+    exit 1
+}
 
 echo -e "${GREEN}Preview:\n${NC}"
 count=0
-while read source destination
-do
-   if [ ! -d "${source}" ]
-   then
-    echo -e "---\n${RED}Error:${NC} Directory ${RED}${source} ${NC}does not exist. Make sure all directories in csv exist. \n---"
-    exit 2
-   fi;
-   echo -e "${ARROW} ./${source}${NC} will be copied to ${GREEN}./${destination}${NC}"
-   let count+=1
-done < $INPUT
+{
+    read
+    while read source destination || [ -n "${source}" ] && [ -n "${destination}" ]; do
+        if [ ! -d "${source}" ]; then
+            echo -e "---\n${RED}Error:${NC} Directory ${RED}${source} ${NC}does not exist. Make sure all directories in csv exist. \n---"
+            exit 2
+        fi
+        echo -e "${ARROW} ./${source}${NC} will be copied to ${GREEN}./${destination}${NC}"
+        let count+=1
+    done
+} <$INPUT
 echo -e "\n${GREEN}Number of samples to be copied: ${count}${NC} \n---"
-while read source destination
-do
+
+read -p "Continue? (y/n)" continue
+case "$continue" in
+y | Y) echo -e "Continuing...\n" ;;
+n | N) exit 0 ;;
+*) echo "invalid" && exit 1 ;;
+esac
+
+while read source destination; do
     # Remove carriage return from destination variable
     destination=$(echo $destination | sed 's/\r//g')
     # Create target directory
     mkdir -p $(dirname $destination)
     # Move sample to destination and commit
-	echo -e "\n\xe2\x88\xb4 Moving ${GREEN}${source}${NC} to ${GREEN}${destination}${NC}\n"
-    git mv $source $destination 
+    echo -e "\n\xe2\x88\xb4 Moving ${GREEN}${source}${NC} to ${GREEN}${destination}${NC}\n"
+    git mv $source $destination
     git commit -m "move ${source} to ${destination}"
     echo -e "\n"
     # Save the ID of the revision with the moved sample for use later in merge
-    saved=`git rev-parse HEAD`
+    saved=$(git rev-parse HEAD)
     # Reset to HEAD, temporarily rename source, and commit
     git reset --hard HEAD^
     git mv ${source} ${source}-copy
@@ -57,4 +68,4 @@ do
     git mv ${source}-copy ${source}
     echo -e "\n"
     git commit -m "merge original copy of ${source}"
-done < $INPUT
+done <$INPUT
