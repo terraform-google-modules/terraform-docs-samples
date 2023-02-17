ef13109781417b7d9d4c27bd59dc8ad711383e79
Betsy Lichtenberg <betsyl@google.com>
2023-02-17 07:50:00 -0800

    Added random prefix and local to cert map name (#304)
    * Skipping tests so that we don't have to comment content
    
    * Uncommented content
    
    * fixed formatting
    
    * Removed unused google_project
    
    * Removed skip test because maybe no longer needed?
    
    * Fixed formatting
    
    * Added random prefix and local to cert map name
    
    * Removed SSL cert from target proxy
    
    * Readded test.yaml because of can't delete certificate map that is referenced by a target proxy error

--- certificate_manager/google_managed_lb_auth/main.tf
+++ certificate_manager/google_managed_lb_auth/main.tf
@@ -51,7 +51,7 @@ resource "google_certificate_manager_certificate" "default" {
 
 # [START certificatemanager_google_managed_lb_auth_map]
 resource "google_certificate_manager_certificate_map" "default" {
-  name        = "certmap1"
+  name        = "${local.name}-certmap1-${random_id.tf_prefix.hex}"
   description = "${local.domain} certificate map"
   labels = {
     "terraform" : true
@@ -99,12 +99,6 @@ resource "google_compute_target_https_proxy" "default" {
   name            = "test-proxy"
   certificate_map = "//certificatemanager.googleapis.com/${google_certificate_manager_certificate_map.default.id}"
   url_map         = google_compute_url_map.default.id
-  ssl_certificates = [
-    google_compute_managed_ssl_certificate.default.name
-  ]
-  depends_on = [
-    google_compute_managed_ssl_certificate.default
-  ]
 }
 # [END certificatemanager_google_managed_lb_auth_target_https_proxy]
 
