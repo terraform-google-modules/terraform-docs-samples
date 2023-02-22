71b886fc060b254e2be86b758c5fd815b7e86e83
Thomas Burke <tomburke@google.com>
2023-02-22 17:59:08 -0500

    broke out terraform configuration doc snippets into their own files

---
+++ cloud_run_configuration_memory_limits/memory_limits.tf
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# Example configuration of a Cloud Run service with memory limit
+
+# [START cloudrun_service_configuration]
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-service-memory-limits"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+
+        resources {
+          limits = {
+            # Memory usage limit (per container)
+            # https://cloud.google.com/run/docs/configuring/memory-limits
+            memory = "512Mi"
+          }
+        }
+      }
+    }
+  }
+}
+# [END cloudrun_service_configuration]
---
+++ cloud_run_configuration_min_instances/min_instances.tf
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# Example configuration of a Cloud Run service with min instances
+
+# [START cloudrun_service_configuration]
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-service-min-instances"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-docker.pkg.dev/cloudrun/container/hello"
+      }
+    }
+    metadata {
+      annotations = {
+        # Min instances
+        # https://cloud.google.com/run/docs/configuring/min-instances
+        "autoscaling.knative.dev/minScale" = 1
+      }
+    }
+  }
+}
+# [END cloudrun_service_configuration]
