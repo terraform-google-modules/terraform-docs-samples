0bf13d08a70f8b1c35a5b5ddb63fab1c10dc881b
Roger Martinez <31829545+rogerthatdev@users.noreply.github.com>
2023-03-16 16:24:56 -0700

    Copy compute, media cdn, and dns samples (#358)
    * move media_cdn_keyset to media_cdn/keyset
    
    * temporary copy of media_cdn_keyset
    
    * merge original copy of media_cdn_keyset
    
    * move media_cdn_quickstart to media_cdn/quickstart
    
    * temporary copy of media_cdn_quickstart
    
    * merge original copy of media_cdn_quickstart
    
    * move media_cdn_security_policies to media_cdn/security_policies
    
    * temporary copy of media_cdn_security_policies
    
    * merge original copy of media_cdn_security_policies
    
    * move compute_disk to compute/disk
    
    * temporary copy of compute_disk
    
    * merge original copy of compute_disk
    
    * move compute_packet_mirroring_full to compute/packet_mirroring_full
    
    * temporary copy of compute_packet_mirroring_full
    
    * merge original copy of compute_packet_mirroring_full
    
    * move compute_reservation to compute/reservation
    
    * temporary copy of compute_reservation
    
    * merge original copy of compute_reservation
    
    * move compute_run_shutdown_scripts to compute/run_shutdown_scripts
    
    * temporary copy of compute_run_shutdown_scripts
    
    * merge original copy of compute_run_shutdown_scripts
    
    * move autoscaler_instance_group to compute/autoscaler_instance_group
    
    * temporary copy of autoscaler_instance_group
    
    * merge original copy of autoscaler_instance_group
    
    * move instance_custom_hostname to compute/instance_custom_hostname
    
    * temporary copy of instance_custom_hostname
    
    * merge original copy of instance_custom_hostname
    
    * move instance_virtual_display_enabled to compute/instance_virtual_display_enabled
    
    * temporary copy of instance_virtual_display_enabled
    
    * merge original copy of instance_virtual_display_enabled
    
    * move os_login to compute/os_login
    
    * temporary copy of os_login
    
    * merge original copy of os_login
    
    * move region_autoscaler_basic to compute/region_autoscaler_basic
    
    * temporary copy of region_autoscaler_basic
    
    * merge original copy of region_autoscaler_basic
    
    * move spot_instance_basic to compute/spot_instance_basic
    
    * temporary copy of spot_instance_basic
    
    * merge original copy of spot_instance_basic
    
    * move dns_managed_zone_basic to dns/managed_zone_basic
    
    * temporary copy of dns_managed_zone_basic
    
    * merge original copy of dns_managed_zone_basic
    
    * move dns_managed_zone_private to dns/managed_zone_private
    
    * temporary copy of dns_managed_zone_private
    
    * merge original copy of dns_managed_zone_private
    
    * move dns_managed_zone_private_forwarding to dns/managed_zone_private_forwarding
    
    * temporary copy of dns_managed_zone_private_forwarding
    
    * merge original copy of dns_managed_zone_private_forwarding
    
    * move dns_managed_zone_private_peering to dns/managed_zone_private_peering
    
    * temporary copy of dns_managed_zone_private_peering
    
    * merge original copy of dns_managed_zone_private_peering
    
    * move dns_managed_zone_quickstart to dns/managed_zone_quickstart
    
    * temporary copy of dns_managed_zone_quickstart
    
    * merge original copy of dns_managed_zone_quickstart
    
    * move dns_managed_zone_service_directory to dns/managed_zone_service_directory
    
    * temporary copy of dns_managed_zone_service_directory
    
    * merge original copy of dns_managed_zone_service_directory
    
    * move dns_policy_basic to dns/policy_basic
    
    * temporary copy of dns_policy_basic
    
    * merge original copy of dns_policy_basic
    
    * move dns_record_set_basic to dns/record_set_basic
    
    * temporary copy of dns_record_set_basic
    
    * merge original copy of dns_record_set_basic
    
    * move dns_response_policy_basic to dns/response_policy_basic
    
    * temporary copy of dns_response_policy_basic
    
    * merge original copy of dns_response_policy_basic
    
    * move dns_response_policy_rule_basic to dns/response_policy_rule_basic
    
    * temporary copy of dns_response_policy_rule_basic
    
    * merge original copy of dns_response_policy_rule_basic

---
+++ compute/run_shutdown_scripts/main.tf
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# [START compute_terraform_shutdown_script_direct_example]
+resource "google_compute_instance" "default" {
+  name         = "instance-name-shutdown-content-directly"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
+  metadata = {
+    # Shuts down Apache server
+    shutdown-script = "#! /bin/bash /etc/init.d/apache2 stop"
+  }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    # A default network is created for all Google Cloud projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+# [END compute_terraform_shutdown_script_direct_example]
+
+
+# [START compute_terraform_shutdown_scriipt_file_example]
+resource "google_compute_instance" "shutdown_content_from_file" {
+  name         = "instance-name-shutdown-content-from-file"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
+  metadata = {
+    # Shuts down Apache server
+    shutdown-script = file("${path.module}/shutdown-script.sh")
+  }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    # A default network is created for all Google Cloud projects
+    network = "default"
+    access_config {
+    }
+  }
+}
+# [END compute_terraform_shutdown_scriipt_file_example]
