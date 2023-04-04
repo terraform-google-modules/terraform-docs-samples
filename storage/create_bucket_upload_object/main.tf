1669ae557a30f7a6fa4f4e5dd9679924d5dcd826
Bonnie Chan <cbonnie@google.com>
2023-04-04 15:32:52 -0400

    Add Terraform sample for creating a bucket and uploading an object

---
+++ storage/create_bucket_upload_object/main.tf
@@ -0,0 +1,21 @@
+# Create new storage bucket in the US multi-region
+# with standard storage
+
+resource "google_storage_bucket" "static" {
+  project       = "<var>PROJECT_ID</var>"
+  name         = "<var>BUCKET_NAME</var>"
+  location      = "US"
+  storage_class = "STANDARD"
+
+  uniform_bucket_level_access = true
+}
+
+# Upload a text file as an object
+# to the storage bucket
+
+resource "google_storage_bucket_object" "default" {
+  name = "<var>OBJECT_NAME</var>"
+  source       = "<var>OBJECT_PATH</var>"
+  content_type = "text/plain"
+  bucket       = google_storage_bucket.static.id
+}
