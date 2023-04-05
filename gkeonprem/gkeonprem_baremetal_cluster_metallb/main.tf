5b0f8519d17c9c342a3b185309b31fdf4f5aadf9
shabirmean <shabirmean@google.com>
2023-04-05 14:54:56 -0400

    chore: add tesk skip file and reorder project id setup

--- gkeonprem/gkeonprem_baremetal_cluster_metallb/main.tf
+++ gkeonprem/gkeonprem_baremetal_cluster_metallb/main.tf
@@ -15,11 +15,17 @@
  */
 
 #[START gkeonprem_bare_metal_cluster_metallb]
+
+# GCP project information is fetched from the 'GOOGLE_CLOUD_PROJECT' environment
+# variable if it is not explicitely set in this data block.
+data "google_project" "project" {
+  project_id = "YOUR_PROJECT_ID"
+}
+
 # Fill in the local variables according to your baremetal environment; you must
 # already have an admin cluster created according to the prerequisites outlined
 # in https://cloud.google.com/anthos/clusters/docs/bare-metal/latest/installing/creating-clusters/create-user-cluster-api#before_you_begin
 locals {
-  project           = "YOUR_PROJECT_ID"
   admin_cluster     = "YOUR_ADMIN_CLUSTER_NAME"
   bmctl_version     = "BMCTL_VERSION"
   control_plane_ip  = "IP_ADDRESS_OF_NEW_CLUSTER_CONTROL_PLANE_NODE" # 10.200.0.4
@@ -33,14 +39,14 @@ locals {
 }
 
 provider "google-private" {
-  project                   = local.project
+  project                   = data.google_project.project.project_id
   region                    = local.region
   gkeonprem_custom_endpoint = local.endpoint
 }
 
 # Enable GKE OnPrem API
 resource "google_project_service" "default" {
-  project            = local.project
+  project            = data.google_project.project.project_id
   service            = "gkeonprem.googleapis.com"
   disable_on_destroy = false
 }
@@ -53,7 +59,7 @@ resource "google_gkeonprem_bare_metal_cluster" "default" {
   depends_on               = [google_project_service.default]
   location                 = local.region
   bare_metal_version       = local.bmctl_version
-  admin_cluster_membership = "projects/${local.project}/locations/global/memberships/${local.admin_cluster}"
+  admin_cluster_membership = "projects/${data.google_project.project.project_id}/locations/global/memberships/${local.admin_cluster}"
   network_config {
     island_mode_cidr {
       service_address_cidr_blocks = ["172.26.0.0/16"]
