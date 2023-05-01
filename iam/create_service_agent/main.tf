a287b6af272b2eca6529c7b532648884055c4c15
melaniedejong <35782177+melaniedejong@users.noreply.github.com>
2023-05-01 15:18:20 -0700

    Reorganize block order/region tags

--- iam/create_service_agent/main.tf
+++ iam/create_service_agent/main.tf
@@ -35,7 +35,15 @@ resource "google_project_service_identity" "default" {
 }
 # [END iam_create_project_level_service_agent]
 
-# [START iam_grant_role_primary_service_agent]
+# [START iam_grant_roles_to_service_agents]
+# Grant the AI Platform Custom Code Service Account the Vertex AI Custom
+# Code Service Agent role (roles/aiplatform.customCodeServiceAgent)
+resource "google_project_iam_member" "custom_code" {
+  project = data.google_project.default.project_id
+  role    = "roles/aiplatform.customCodeServiceAgent"
+  member  = "serviceAccount:service-${data.google_project.default.number}@gcp-sa-aiplatform-cc.iam.gserviceaccount.com"
+}
+
 # Grant the primary aiplatform.googleapis.com service agent (AI Platform
 # Service Agent) the Vertex AI Service Agent role
 # (roles/aiplatform.serviceAgent)
@@ -44,14 +52,4 @@ resource "google_project_iam_member" "primary" {
   role    = "roles/aiplatform.serviceAgent"
   member  = "serviceAccount:${google_project_service_identity.default.email}"
 }
-# [END iam_grant_role_primary_service_agent]
-
-# [START iam_grant_role_non_primary_service_agent]
-# Grant the AI Platform Custom Code Service Account the Vertex AI Custom
-# Code Service Agent role (roles/aiplatform.customCodeServiceAgent)
-resource "google_project_iam_member" "custom_code" {
-  project = data.google_project.default.project_id
-  role    = "roles/aiplatform.customCodeServiceAgent"
-  member  = "serviceAccount:service-${data.google_project.default.number}@gcp-sa-aiplatform-cc.iam.gserviceaccount.com"
-}
-# [END iam_grant_role_non_primary_service_agent]
+# [END iam_grant_roles_to_service_agents]
