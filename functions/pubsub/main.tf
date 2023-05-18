4d0db9e164603b470161b8797b7b79c982baf2b2
Sampath Kumar <sampathm@google.com>
2023-05-18 16:57:39 +0200

    feat: Cloud functions code cleanup & refactor (#420)
    * feat: replace a zip with its content files
    
    * feat: Replace zip with its files to increase transparency
    * chore: Use `default` as resource names
    
    * feat: replace a zip with its content files
    
    * feat: Replace zip with its files to increase transparency
    * chore: Use `default` as resource names
    
    * feat: replace a zip with its content files
    
    * feat: Replace zip with its files to increase transparency
    * chore: Use `default` as resource names
    
    * chore: Add missing licence headers and add missing empty line at the EOF.

--- functions/pubsub/main.tf
+++ functions/pubsub/main.tf
@@ -29,28 +29,34 @@ resource "random_id" "bucket_prefix" {
 }
 
 
-resource "google_service_account" "account" {
-  account_id   = "gcf-sa"
+resource "google_service_account" "default" {
+  account_id   = "test-gcf-sa"
   display_name = "Test Service Account"
 }
 
-resource "google_pubsub_topic" "topic" {
+resource "google_pubsub_topic" "default" {
   name = "functions2-topic"
 }
 
-resource "google_storage_bucket" "bucket" {
+resource "google_storage_bucket" "default" {
   name                        = "${random_id.bucket_prefix.hex}-gcf-source" # Every bucket name must be globally unique
   location                    = "US"
   uniform_bucket_level_access = true
 }
 
-resource "google_storage_bucket_object" "object" {
-  name   = "function-source.zip"
-  bucket = google_storage_bucket.bucket.name
-  source = "function-source.zip" # Add path to the zipped function source code
+data "archive_file" "default" {
+  type        = "zip"
+  output_path = "/tmp/function-source.zip"
+  source_dir  = "function-source/"
 }
 
-resource "google_cloudfunctions2_function" "function" {
+resource "google_storage_bucket_object" "default" {
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.default.name
+  source = data.archive_file.default.output_path # Path to the zipped function source code
+}
+
+resource "google_cloudfunctions2_function" "default" {
   name        = "function"
   location    = "us-central1"
   description = "a new function"
@@ -63,8 +69,8 @@ resource "google_cloudfunctions2_function" "function" {
     }
     source {
       storage_source {
-        bucket = google_storage_bucket.bucket.name
-        object = google_storage_bucket_object.object.name
+        bucket = google_storage_bucket.default.name
+        object = google_storage_bucket_object.default.name
       }
     }
   }
@@ -79,13 +85,13 @@ resource "google_cloudfunctions2_function" "function" {
     }
     ingress_settings               = "ALLOW_INTERNAL_ONLY"
     all_traffic_on_latest_revision = true
-    service_account_email          = google_service_account.account.email
+    service_account_email          = google_service_account.default.email
   }
 
   event_trigger {
     trigger_region = "us-central1"
     event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
-    pubsub_topic   = google_pubsub_topic.topic.id
+    pubsub_topic   = google_pubsub_topic.default.id
     retry_policy   = "RETRY_POLICY_RETRY"
   }
 }
