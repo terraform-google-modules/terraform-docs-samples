29d7fcf89b8b44223164567c654108af1c9003db
Betsy Lichtenberg <betsyl@google.com>
2023-05-30 14:53:26 -0700

    Added missing underscore

--- certificate_manager/dns_managed_zone_with_wildcard_certificate/main.tf
+++ certificate_manager/dns_managed_zone_with_wildcard_certificate/main.tf
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-# [START certificatemanagerdns_managed_zone_with_wildcard_certificate_parent_tag]
+# [START certificatemanager_dns_managed_zone_with_wildcard_certificate_parent_tag]
 locals {
   domain = "example.me"
   name   = "prefixname"
@@ -123,4 +123,4 @@ output "domain_name_servers" {
 output "certificate_map" {
   value = google_certificate_manager_certificate_map.certificate_map.id
 }
-# [END certificatemanagerdns_managed_zone_with_wildcard_certificate_parent_tag]
+# [END certificatemanager_dns_managed_zone_with_wildcard_certificate_parent_tag]
