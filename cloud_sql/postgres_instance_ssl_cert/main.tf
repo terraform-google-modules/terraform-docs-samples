6b49babc777f4a894fa999cf565cccb1aca9fbad
Betsy Lichtenberg <betsyl@google.com>
2023-06-01 11:47:08 -0700

    Split of Cloud SQL samples

---
+++ cloud_sql/postgres_instance_ssl_cert/main.tf
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# [START cloud_sql_postgres_instance_require_ssl]
+resource "google_sql_database_instance" "postgres_instance" {
+  name             = "postgres-instance"
+  region           = "asia-northeast1"
+  database_version = "postgres_14"
+  settings {
+    tier = "db-custom-2-7680"
+    ip_configuration {
+      require_ssl = "true"
+    }
+  }
+  # set `deletion_protection` to true, will ensure that one cannot accidentally delete this instance by
+  # use of Terraform whereas `deletion_protection_enabled` flag protects this instance at the GCP level.
+  deletion_protection = false
+}
+# [END cloud_sql_postgres_instance_require_ssl]
+
+# [START cloud_sql_postgres_instance_ssl_cert]
+resource "google_sql_ssl_cert" "postgres_client_cert" {
+  common_name = "postgres_common_name"
+  instance    = google_sql_database_instance.postgres_instance.name
+}
+# [END cloud_sql_postgres_instance_ssl_cert]
