6829a0d223d675ff433a911c298fb9424d890f17
Christa Carpentiere <chriscar@google.com>
2023-06-21 15:38:39 +0000

    feat(bigquery): Add examples for creating a table

--- bigquery/bigquery_create_table_iam/main.tf
+++ bigquery/bigquery_create_table_iam/main.tf
@@ -30,8 +30,9 @@ resource "google_bigquery_dataset" "default" {
 }
 
 resource "google_bigquery_table" "default" {
-  dataset_id = google_bigquery_dataset.default.dataset_id
-  table_id   = "mytable"
+  dataset_id          = google_bigquery_dataset.default.dataset_id
+  table_id            = "mytable"
+  deletion_protection = false # set to "true" in production
 
   schema = <<EOF
 [
