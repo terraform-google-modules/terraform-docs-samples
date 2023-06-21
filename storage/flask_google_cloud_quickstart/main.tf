61f969a01eb00620df2b3b990370ece56923e7f1
Betsy Lichtenberg <betsyl@google.com>
2023-06-21 07:45:10 -0700

    added depends-on for iam member

--- storage/flask_google_cloud_quickstart/main.tf
+++ storage/flask_google_cloud_quickstart/main.tf
@@ -132,6 +132,9 @@ resource "google_storage_bucket" "default" {
   encryption {
     default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
   }
+  depends_on = [
+    google_project_iam_member.default
+  ]
 }
 # [END storage_bucket_tf_with_versioning]
 # [END storage_flask_google_cloud_quickstart_parent_tag]
