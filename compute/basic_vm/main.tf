721f40c6a2ef039bb280cde0b12d955483868aad
Jennifer Davis <sigje@google.com>
2023-06-29 12:36:47 -0700

    fix: remove new sample from basic_vm
    this eliminates the duplicate resource name "default" and also complies to the one sample per file.

--- compute/basic_vm/main.tf
+++ compute/basic_vm/main.tf
@@ -75,31 +75,3 @@ resource "google_compute_instance" "custom_subnet" {
 }
 # [END compute_instances_create_with_subnet]
 # [END compute_basic_vm_parent_tag]
-
-# [START compute_instances_create_with_local_ssd]
-
-# Create a VM with a local SSD for temporary storage use cases
-
-resource "google_compute_instance" "default" {
-  name         = "my-vm-instance-with-scratch"
-  machine_type = "n2-standard-8"
-  zone         = "us-central1-a"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  # Local SSD interface type; NVME for image with optimized NVMe drivers or SCSI
-  # Local SSD are 375 GiB in size
-  scratch_disk {
-    interface = "SCSI"
-  }
-
-  network_interface {
-    network = "default"
-    access_config {}
-  }
-}
-# [END compute_instances_create_with_local_ssd]
