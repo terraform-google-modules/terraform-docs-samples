cbf51d8ff5351317a89e8ae6ab16487a78ac7481
Ronnie Phillips <vpronnie@google.com>
2023-08-24 13:16:26 -0700

    Rename resource and add explanatory comment for basic instance

--- google_looker_instance/looker_instance_basic/main.tf
+++ google_looker_instance/looker_instance_basic/main.tf
@@ -12,8 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
- 
+ */ 
 # [START looker_google_looker_instance_basic]
 # Creates a Standard edition Looker (Google Cloud core) instance with basic functionality enabled.
 resource "google_looker_instance" "main" {
