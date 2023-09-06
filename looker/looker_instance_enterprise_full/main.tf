0bde7c11a485cfce83cd6f8bda85e1a03022b632
rogerthatdev <roger.that.dev@gmail.com>
2023-09-06 08:47:36 -0700

    remove lifecycle option

--- looker/looker_instance_enterprise_full/main.tf
+++ looker/looker_instance_enterprise_full/main.tf
@@ -75,10 +75,6 @@ resource "google_kms_key_ring" "main" {
 resource "google_kms_crypto_key" "main" {
   name     = "crypto-key-example"
   key_ring = google_kms_key_ring.main.id
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "google_service_networking_connection" "main" {
