54e66d1531d936d775561b88a687faa2600becb2
Katie McLaughlin <katie@glasnt.com>
2023-09-08 07:56:18 +1000

    fix: introduce globally unique bucket names

--- eventarc/basic/main.tf
+++ eventarc/basic/main.tf
@@ -63,9 +63,14 @@ resource "google_project_iam_member" "runinvoker" {
 
 
 # [START storage_terraform_eventarc_cloudrun]
+# Cloud Storage bucket names must be globally unique
+resource "random_id" "bucket_name_suffix" {
+  byte_length = 4
+}
+
 # Create a Cloud Storage bucket
 resource "google_storage_bucket" "default" {
-  name          = "trigger-cloudrun-${data.google_project.project.name}"
+  name          = "trigger-cloudrun-${data.google_project.project.name}-${random_id.bucket_name_suffix.hex}"
   location      = google_cloud_run_v2_service.default.location
   force_destroy = true
 
--- eventarc/workflows/main.tf
+++ eventarc/workflows/main.tf
@@ -62,9 +62,14 @@ resource "google_project_iam_member" "eventreceiver" {
 
 
 # [START storage_terraform_eventarc_workflows]
+# Cloud Storage bucket names must be globally unique
+resource "random_id" "bucket_name_suffix" {
+  byte_length = 4
+}
+
 # Create a Cloud Storage bucket
 resource "google_storage_bucket" "default" {
-  name          = "trigger-workflows-${data.google_project.project.name}"
+  name          = "trigger-workflows-${data.google_project.project.name}-${random_id.bucket_name_suffix.hex}"
   location      = google_workflows_workflow.default.region
   force_destroy = true
 
