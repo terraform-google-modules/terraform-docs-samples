81ccc43d792fd6e2c05817500a8ce4e296c23c59
Sahana C Prabhakar <sahanacp@google.com>
2023-09-13 18:43:28 +0530

    Fix lint issues inclusion in C.G.C. documentation

--- lb/cross_reg_int_app_lb_mig_backend/main.tf
+++ lb/cross_reg_int_app_lb_mig_backend/main.tf
@@ -172,13 +172,9 @@ resource "google_compute_instance_template" "instance_template_a" {
       apt-get update
       apt-get install -y nginx-light jq
 
-      NAME=$(curl -H "Metadata-Flavor: Google" 
-"http://metadata.google.internal/computeMetadata/v1/instance/hostname")
-      IP=$(curl -H "Metadata-Flavor: Google" 
-"http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
-      METADATA=$(curl -f -H "Metadata-Flavor: Google" 
-"http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=True" 
-| jq 'del(.["startup-script"])')
+      NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
+      IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
+      METADATA=$(curl -f -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=True" | jq 'del(.["startup-script"])')
 
       cat <<EOF > /var/www/html/index.html
       <pre>
@@ -226,13 +222,9 @@ resource "google_compute_instance_template" "instance_template_b" {
       apt-get update
       apt-get install -y nginx-light jq
 
-      NAME=$(curl -H "Metadata-Flavor: Google" 
-"http://metadata.google.internal/computeMetadata/v1/instance/hostname")
-      IP=$(curl -H "Metadata-Flavor: Google" 
-"http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
-      METADATA=$(curl -f -H "Metadata-Flavor: Google" 
-"http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=True" 
-| jq 'del(.["startup-script"])')
+      NAME=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/hostname")
+      IP=$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip")
+      METADATA=$(curl -f -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=True" | jq 'del(.["startup-script"])')
 
       cat <<EOF > /var/www/html/index.html
       <pre>
