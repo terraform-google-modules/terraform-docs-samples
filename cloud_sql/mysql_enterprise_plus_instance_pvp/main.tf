3c51b42fe66d6f4b0d4ae5ccda6cb8efb5ddfa52
ibhaskar2 <ibhaskar@google.com>
2023-10-11 12:27:42 +0000

    feat(cloudsql):Added postgres and mysql samples for Enterprise_plus edition

---
+++ cloud_sql/mysql_enterprise_plus_instance_pvp/main.tf
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# [START cloud_sql_mysql_enterprise_plus_instance_pvp]
+resource "google_sql_database_instance" "mysql_pvp_enterprise_plus_instance_name" {
+  name             = "mysql-pvp-enterprise-plus-instance-name"
+  region           = "us-central1"
+  database_version = "MYSQL_8_0"
+  root_password    = "abcABC123!"
+  settings {
+    tier    = "db-perf-optimized-N-2"
+    edition = "ENTERPRISE_PLUS"
+    password_validation_policy {
+      min_length                  = 6
+      complexity                  = "COMPLEXITY_DEFAULT"
+      reuse_interval              = 2
+      disallow_username_substring = true
+      enable_password_policy      = true
+    }
+  }
+  # set `deletion_protection` to true, will ensure that one cannot accidentally delete this instance by
+  # use of Terraform whereas `deletion_protection_enabled` flag protects this instance at the GCP level.
+  deletion_protection = false
+}
+# [END cloud_sql_mysql_enterprise_plus_instance_pvp]
