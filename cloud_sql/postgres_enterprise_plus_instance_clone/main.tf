1a732d44d43e05b45ef45dd1b85e29e8240a78a0
ibhaskar2 <ibhaskar@google.com>
2023-10-11 12:27:42 +0000

    Added postgres and mysql samples for Enterprise_plus edition

---
+++ cloud_sql/postgres_enterprise_plus_instance_clone/main.tf
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# [START cloud_sql_postgres_enterprise_plus_instance_clone_parent_tag]
+# [START cloud_sql_postgres_enterprise_plus_instance_source]
+resource "google_sql_database_instance" "source" {
+  name             = "postgres-enterprise-plus-instance-source-name"
+  region           = "us-central1"
+  database_version = "POSTGRES_15"
+  settings {
+    tier    = "db-perf-optimized-N-2"
+    edition = "ENTERPRISE_PLUS"
+  }
+  # set `deletion_protection` to true, will ensure that one cannot accidentally delete this instance by
+  # use of Terraform whereas `deletion_protection_enabled` flag protects this instance at the GCP level.
+  deletion_protection = false
+}
+# [END cloud_sql_postgres_enterprise_plus_instance_source]
+
+# [START cloud_sql_postgres_enterprise_plus_instance_clone]
+resource "google_sql_database_instance" "clone" {
+  name             = "postgres-enterprise-plus-instance-clone-name"
+  region           = "us-central1"
+  database_version = "POSTGRES_15"
+  settings {
+    tier    = "db-perf-optimized-N-2"
+    edition = "ENTERPRISE_PLUS"
+  }
+  clone {
+    source_instance_name = google_sql_database_instance.source.id
+  }
+  # set `deletion_protection` to true, will ensure that one cannot accidentally delete this instance by
+  # use of Terraform whereas `deletion_protection_enabled` flag protects this instance at the GCP level.
+  deletion_protection = false
+}
+# [END cloud_sql_postgres_enterprise_plus_instance_clone]
+# [END cloud_sql_postgres_enterprise_plus_instance_clone_parent_tag]
