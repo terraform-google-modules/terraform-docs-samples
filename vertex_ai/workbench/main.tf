6ff7d14e86dbe3adc300b90eb858d683e000b19e
Bhargav Chereddy <bcreddy@google.com>
2024-01-25 00:21:44 +0000

    Doc samples on how to update metadata on workbench instances.

--- vertex_ai/workbench/main.tf
+++ vertex_ai/workbench/main.tf
@@ -33,3 +33,54 @@ resource "google_workbench_instance" "default" {
   }
 }
 # [END aiplatform_workbench_basic_gpu_instance]
+
+# [START aiplatform_workbench_basic_metadata]
+resource "google_workbench_instance" "default" {
+  name     = "workbench-instance-example"
+  location = "us-central1-a"
+
+  gce_setup {
+    machine_type = "n1-standard-1"
+    vm_image {
+      project = "deeplearning-platform-release"
+      family  = "tf-latest-gpu"
+    }
+    metadata = {
+      key = "value"
+    }
+  }
+}
+# [END aiplatform_workbench_basic_metadata]
+
+# [START aiplatform_workbench_updated_metadata]
+resource "google_workbench_instance" "default" {
+  name     = "workbench-instance-example"
+  location = "us-central1-a"
+
+  gce_setup {
+    machine_type = "n1-standard-1"
+    vm_image {
+      project = "deeplearning-platform-release"
+      family  = "tf-latest-gpu"
+    }
+    metadata = {
+      key = "updated_value"
+    }
+  }
+}
+# [END aiplatform_workbench_updated_metadata]
+
+# [START aiplatform_workbench_removed_metadata]
+resource "google_workbench_instance" "default" {
+  name     = "workbench-instance-example"
+  location = "us-central1-a"
+
+  gce_setup {
+    machine_type = "n1-standard-1"
+    vm_image {
+      project = "deeplearning-platform-release"
+      family  = "tf-latest-gpu"
+    }
+  }
+}
+# [END aiplatform_workbench_removed_metadata]
