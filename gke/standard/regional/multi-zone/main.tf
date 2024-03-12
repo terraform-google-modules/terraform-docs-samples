ee7189505b7ac8a05696ca632dfac0ea159f5a21
Andrew Peabody <andrewpeabody@google.com>
2024-03-12 08:20:11 -0700

    feat: add gke standard samples (#613)

---
+++ gke/standard/regional/multi-zone/main.tf
@@ -0,0 +1,28 @@
+/**
+* Copyright 2024 Google LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+# [START gke_standard_regional_multi_zone]
+resource "google_container_cluster" "default" {
+  name               = "gke-standard-regional-multi-zone"
+  location           = "us-central1"
+  node_locations     = ["us-central1-b", "us-central1-c"]
+  initial_node_count = 2
+
+  # Set `deletion_protection` to `true` will ensure that one cannot
+  # accidentally delete this instance by use of Terraform.
+  deletion_protection = false
+}
+# [END gke_standard_regional_multi_zone]
