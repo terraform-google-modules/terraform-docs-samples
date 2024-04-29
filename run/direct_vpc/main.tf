901330121311c50af5c61b7e76380ef01de7421e
Katie McLaughlin <katie@glasnt.com>
2024-04-30 09:55:45 +1000

    Update run/direct_vpc/main.tf

--- run/direct_vpc/main.tf
+++ run/direct_vpc/main.tf
@@ -25,11 +25,11 @@ resource "google_cloud_run_v2_service" "default" {
     containers {
       image = "us-docker.pkg.dev/cloudrun/container/hello"
     }
-    vpc_access{
+    vpc_access {
       network_interfaces {
-        network = "default"
+        network    = "default"
         subnetwork = "default"
-        tags = ["tag1", "tag2", "tag3"]
+        tags       = ["tag1", "tag2", "tag3"]
       }
     }
   }
