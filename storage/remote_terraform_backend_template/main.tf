67a23374b2a38d6c76be3ef366cf192ff41f8946
Marco Ferrari <ferrari.marco@gmail.com>
2024-05-16 01:14:57 +0200

    feat: automated terraform remote backend template (#660)
    * feat: automated terraform remote backend template
    
    In this example, we show how to provision a Cloud Storage bucket and
    then generate a Terraform backend configuration file.
    
    * correct spelling
    
    * Move additional instructions to README
    
    * style: use 'default' for resource names (unless multiple of the same type)
    
    * Due to complexity of template, do not apply/destory
    
    * add README meta to satisfy lint check
    
    ---------
    
    Co-authored-by: Katie McLaughlin <katie@glasnt.com>

---
+++ storage/remote_terraform_backend_template/main.tf
@@ -0,0 +1,52 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START storage_remote_terraform_backend_template]
+# [START storage_bucket_tf_with_versioning_pap_uap_no_destroy]
+resource "random_id" "default" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name     = "${random_id.default.hex}-terraform-remote-backend"
+  location = "US"
+
+  force_destroy               = false
+  public_access_prevention    = "enforced"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+}
+# [END storage_bucket_tf_with_versioning_pap_uap_no_destroy]
+
+# [START storage_remote_backend_local_file]
+resource "local_file" "default" {
+  file_permission = "0644"
+  filename        = "${path.module}/backend.tf"
+
+  # You can store the template in a file and use the templatefile function for
+  # more modularity, if you prefer, instead of storing the template inline as
+  # we do here.
+  content = <<-EOT
+  terraform {
+    backend "gcs" {
+      bucket = "${google_storage_bucket.default.name}"
+    }
+  }
+  EOT
+}
+# [END storage_remote_backend_local_file]
+# [END storage_remote_terraform_backend_template]
