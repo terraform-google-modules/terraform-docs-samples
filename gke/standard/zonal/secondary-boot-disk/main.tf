53390a037746591f8878140c6efdb439d1bdc0f9
Aston Yong <astonyong@google.com>
2024-05-30 22:43:16 +0000

    Add secondary boot disk sample for preloaded data use case.

--- gke/standard/zonal/secondary-boot-disk/main.tf
+++ gke/standard/zonal/secondary-boot-disk/main.tf
@@ -45,3 +45,34 @@ resource "google_container_node_pool" "default" {
   }
 }
 # [END gke_standard_zonal_secondary_boot_disk]
+
+# [START gke_standard_zonal_secondary_boot_disk_data]
+resource "google_container_cluster" "default" {
+  name               = "default"
+  location           = "us-central1-a"
+  initial_node_count = 1
+  # Set `min_master_version` because secondary_boot_disks require GKE 1.28.3-gke.106700 or later
+  min_master_version = "1.28"
+  # Set `deletion_protection` to `true` will ensure that one cannot
+  # accidentally delete this instance by use of Terraform.
+  deletion_protection = false
+}
+
+resource "google_container_node_pool" "default" {
+  name               = "default"
+  location           = "us-central1-a"
+  cluster            = google_container_cluster.default.name
+  initial_node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    image_type   = "COS_CONTAINERD"
+    gcfs_config {
+      enabled = true
+    }
+    secondary_boot_disks {
+      disk_image = ""
+    }
+  }
+}
+# [END gke_standard_zonal_secondary_boot_disk_data]
