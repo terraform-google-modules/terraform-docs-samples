6de11731e71a74561f76db8fb0f6de7fa7be1da7
Andrew Peabody <andrewpeabody@google.com>
2024-07-23 22:52:20 +0000

    feat(GKE): add binary-authorization enforce sample

---
+++ gke/standard/regional/binary-authorization/main.tf
@@ -0,0 +1,31 @@
+/**
+* Copyright 2024 Google LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+# [START gke_standard_regional_binary-authorization-enforce]
+resource "google_container_cluster" "enforce" {
+  name               = "gke-standard-regional-binauthz-enforce"
+  location           = "us-west1"
+  initial_node_count = 1
+
+  binary_authorization {
+    evaluation_mode = "PROJECT_SINGLETON_POLICY_ENFORCE"
+  }
+
+  # Set `deletion_protection` to `true` will ensure that one cannot
+  # accidentally delete this instance by use of Terraform.
+  deletion_protection = false
+}
+# [END gke_standard_regional_binary-authorization-enforce]
