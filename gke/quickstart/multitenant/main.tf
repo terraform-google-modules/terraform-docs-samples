3a97bd3ba146aba80629090867b92962dcef095a
Andrew Peabody <andrewpeabody@google.com>
2024-07-26 14:19:26 -0700

    Update main.tf

--- gke/quickstart/multitenant/main.tf
+++ gke/quickstart/multitenant/main.tf
@@ -176,7 +176,7 @@ resource "google_project_iam_member" "cloudsql" {
   role    = "roles/cloudsql.client"
   member  = "serviceAccount:${data.google_project.default.project_id}.svc.id.goog[${each.value}-team/default]"
 
-depends_on = [google_container_cluster.default]
+  depends_on = [google_container_cluster.default]
 }
 
 data "google_client_config" "default" {}
