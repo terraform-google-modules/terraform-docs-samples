99022caf9f0ab358be5fd9d396a491581843e423
pratikmore0290 <morep@google.com>
2024-08-21 22:36:51 +0000

    feat(bigquery): Add example for creating a tag key

---
+++ bigquery/bigquery_tags_tag_key/main.tf
@@ -0,0 +1,27 @@
+/**
+* Copyright 2024 Google LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+# [START bigquery_tags_tag_key]
+resource "google_tags_tag_key" "env_tag_key" {
+  parent     = "projects/myproject1-381000"
+  short_name = "env"
+}
+
+resource "google_tags_tag_key" "department_tag_key" {
+  parent     = "projects/myproject1-381000"
+  short_name = "department"
+}
+# [END bigquery_tags_tag_key]
