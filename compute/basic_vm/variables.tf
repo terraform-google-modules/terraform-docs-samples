4b1e28a0f13e7ab4ade8fa80c1323bd599e9a194
mbazzii <mohamadbazzi3@gmail.com>
2024-11-19 17:53:55 -0500

    Update variables.tf
    Deploying compute engine using variables.tf instead of coded values in main.tf

--- compute/basic_vm/variables.tf
+++ compute/basic_vm/variables.tf
@@ -1,1 +1,29 @@
+variable "project" {
+  description = "The project ID to host the cluster in"
+  type        = string
+  default     = "polar-cyclist-244407"
+}
 
+variable "location" {
+  description = "The location (region or zone) to host the cluster in"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "name" {
+  description = "The name of the cluster"
+  type        = string
+  name        = "hamoudi_terraform1"
+}
+
+variable "network" {
+  description = "A reference (self link) to the VPC network to host the cluster in"
+  type        = string
+  name        = "default"
+}
+
+variable "subnetwork" {
+  description = "A reference (self link) to the subnetwork to host the cluster in"
+  type        = string
+  name        = "default"
+}
