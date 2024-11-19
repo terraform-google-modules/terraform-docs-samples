214b75b2e13b11284f07c17dd1c29f507be8941e
mbazzii <mohamadbazzi3@gmail.com>
2024-11-19 18:05:26 -0500

    Update main.tf
    removed other examples, and kept the basic VM configuration.

--- compute/basic_vm/main.tf
+++ compute/basic_vm/main.tf
@@ -21,57 +21,19 @@
 # in the `default` VPC network and subnet
 
 resource "google_compute_instance" "default" {
-  name         = "my-vm"
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.location
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+      image = var.image
     }
   }
 
   network_interface {
-    network = "default"
+    network = var.network
     access_config {}
   }
 }
 # [END compute_instances_create]
-
-# [START vpc_compute_basic_vm_custom_vpc_network]
-resource "google_compute_network" "custom" {
-  name                    = "my-network"
-  auto_create_subnetworks = false
-}
-# [END vpc_compute_basic_vm_custom_vpc_network]
-
-# [START vpc_compute_basic_vm_custom_vpc_subnet]
-resource "google_compute_subnetwork" "custom" {
-  name          = "my-subnet"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "europe-west1"
-  network       = google_compute_network.custom.id
-}
-# [END vpc_compute_basic_vm_custom_vpc_subnet]
-
-# [START compute_instances_create_with_subnet]
-
-# Create a VM in a custom VPC network and subnet
-
-resource "google_compute_instance" "custom_subnet" {
-  name         = "my-vm-instance"
-  tags         = ["allow-ssh"]
-  zone         = "europe-west1-b"
-  machine_type = "e2-small"
-  network_interface {
-    network    = google_compute_network.custom.id
-    subnetwork = google_compute_subnetwork.custom.id
-  }
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-12"
-    }
-  }
-}
-# [END compute_instances_create_with_subnet]
-# [END compute_basic_vm_parent_tag]
