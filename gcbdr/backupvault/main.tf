e8f6405f2d5b166652c81a4273613d7676a7ccd5
Niharika <35183015+niharika-98@users.noreply.github.com>
2024-11-21 04:40:28 +0530

    Update gcbdr/backupvault/main.tf
    Co-authored-by: Katie McLaughlin <katie@glasnt.com>

--- gcbdr/backupvault/main.tf
+++ gcbdr/backupvault/main.tf
@@ -20,7 +20,7 @@ resource "google_backup_dr_backup_vault" "default" {
   provider                                   = google-beta
   location                                   = "us-central1"
   backup_vault_id                            = "bv-1"
-  description                                = "This is a second backup vault built by Terraform."
+  description                                = "This vault is provisioned by Terraform."
   backup_minimum_enforced_retention_duration = "100000s"
 
   labels = {
