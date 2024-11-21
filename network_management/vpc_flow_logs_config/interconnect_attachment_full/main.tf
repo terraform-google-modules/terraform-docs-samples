5ecce3b52c721777c40666f89f375adffb7e760a
alkobi-google <alkobi@google.com>
2024-11-21 12:59:57 +0200

    Prepare VPC Flow Logs for inclusion in C.G.C. documentation

---
+++ network_management/vpc_flow_logs_config/interconnect_attachment_full/main.tf
@@ -0,0 +1,31 @@
+/**
+* Copyright 2024 Google LLC
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+# [START vpcflowlogs_create_vpc_flow_logs_config_over_interconnect_attachment_with_parameters]
+# google_network_management_vpc_flow_logs_config.vpc_fl_config will be created
+resource "google_network_management_vpc_flow_logs_config" "vpc_fl_config" {
+  aggregation_interval    = "INTERVAL_10_MIN"
+  description             = "VPC Flow Logs over a VPN Gateway."
+  flow_sampling           = 0.7
+  interconnect_attachment = "projects/example_project/regions/us-central1/interconnectAttachments/example_interconnect"
+  location                = "global"
+  metadata                = "INCLUDE_ALL_METADATA"
+  project                 = "example_project"
+  provider                = google-beta
+  state                   = "ENABLED"
+  vpc_flow_logs_config_id = "example-config-id"
+}
+# [END vpcflowlogs_create_vpc_flow_logs_config_over_interconnect_attachment_with_parameters]
