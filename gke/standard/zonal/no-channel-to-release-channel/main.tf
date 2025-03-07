c80cf41a02d7d0aeb428a1612c6452f77c9b776f
Cordelia Link <cslink@google.com>
2025-03-07 22:03:17 +0000

    Terraform samples for switching no channel cluster to a release channel.

---
+++ gke/standard/zonal/no-channel-to-release-channel/main.tf
@@ -0,0 +1,173 @@
+[START gke_standard_release_channel_none]
+resource "google_container_cluster" "rc_none" {
+  name     = "cluster-zonal-example-none-to-rc"
+  location = "us-central1-a"
+
+  release_channel {
+    channel = "UNSPECIFIED"
+  }
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  # Set `deletion_protection` to `true` will ensure that one cannot
+  # accidentally delete this instance by use of Terraform.
+  deletion_protection = false
+}
+
+resource "google_container_node_pool" "rc_none" {
+  name    = "cluster-zonal-example-none-to-rc-node-pool"
+  cluster = google_container_cluster.rc_none.name
+
+  node_count = 2
+
+  management {
+    auto_repair  = false
+    auto_upgrade = false
+  }
+}
+[END gke_standard_release_channel_none]
+
+[START gke_standard_release_channel_no_upgrade]
+resource "google_container_cluster" "no_upgrade" {
+  name     = "cluster-zonal-example-none-to-rc"
+  location = "us-central1-a"
+
+  release_channel {
+    channel = "UNSPECIFIED"
+  }
+
+  maintenance_policy {
+    # A recurring window must be set to use maintenance exclusions in Terraform.
+    recurring_window {
+      start_time = "2025-02-11T19:16:39Z"
+      end_time   = "2025-02-11T23:16:39Z"
+      recurrence = "FREQ=DAILY"
+    }
+    maintenance_exclusion {
+      exclusion_name = "no_upgrades_exclusion"
+      start_time     = "2025-02-11T23:16:39Z"
+      # NO_UPGRADES exclusions are limited to 30 days.
+      end_time = "2025-03-11T23:16:39Z"
+      exclusion_options {
+        scope = "NO_UPGRADES"
+      }
+    }
+  }
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  # Set `deletion_protection` to `true` will ensure that one cannot
+  # accidentally delete this instance by use of Terraform.
+  deletion_protection = false
+}
+
+resource "google_container_node_pool" "no_upgrade" {
+  name    = "cluster-zonal-example-none-to-rc-node-pool"
+  cluster  = google_container_cluster.no_upgrade.name
+
+  node_count = 2
+
+  management {
+    auto_repair  = false
+    auto_upgrade = false
+  }
+}
+[END gke_standard_release_channel_no_upgrade]
+
+[START gke_standard_release_channel_rc_regular]
+resource "google_container_cluster" "rc_regular" {
+  name     = "cluster-zonal-example-none-to-rc"
+  location = "us-central1-a"
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  maintenance_policy {
+    recurring_window {
+      start_time = "2025-02-11T19:16:39Z"
+      end_time   = "2025-02-11T23:16:39Z"
+      recurrence = "FREQ=DAILY"
+    }
+    maintenance_exclusion {
+      exclusion_name = "no_upgrades_exclusion"
+      start_time     = "2025-02-11T23:16:39Z"
+      end_time       = "2025-03-11T23:16:39Z"
+      exclusion_options {
+        scope = "NO_UPGRADES"
+      }
+    }
+  }
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  # Set `deletion_protection` to `true` will ensure that one cannot
+  # accidentally delete this instance by use of Terraform.
+  deletion_protection = false
+}
+
+resource "google_container_node_pool" "rc_regular" {
+  name    = "cluster-zonal-example-none-to-rc-node-pool"
+  cluster  = google_container_cluster.rc_regular.name
+
+  node_count = 2
+
+  management {
+    # Auto-repair and auto-upgrade should be set to true when moving to a
+    # release channel.
+    auto_repair  = true
+    auto_upgrade = true
+  }
+}
+[END gke_standard_release_channel_rc_regular]
+
+[START gke_standard_release_channel_no_minor_or_node_upgrade]
+resource "google_container_cluster" "no_minor_or_node_upgrade" {
+  name     = "cluster-zonal-example-none-to-rc"
+  location = "us-central1-a"
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  maintenance_policy {
+    recurring_window {
+      start_time = "2025-02-11T19:16:39Z"
+      end_time   = "2025-02-11T23:16:39Z"
+      recurrence = "FREQ=DAILY"
+    }
+    maintenance_exclusion {
+      exclusion_name = "no_minor_or_node_upgrades_exclusion"
+      start_time     = "2025-02-11T23:16:39Z"
+      # This the end of standard support for 1.31.
+      end_time = "2025-12-22T00:00:00Z"
+      exclusion_options {
+        scope = "NO_MINOR_OR_NODE_UPGRADES"
+      }
+    }
+  }
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  # Set `deletion_protection` to `true` will ensure that one cannot
+  # accidentally delete this instance by use of Terraform.
+  deletion_protection = false
+}
+
+resource "google_container_node_pool" "no_minor_or_node_upgrade" {
+  name    = "cluster-zonal-example-none-to-rc-node-pool"
+  cluster  = google_container_cluster.no_minor_or_node_upgrade.name
+
+  node_count = 2
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+}
+[END gke_standard_release_channel_no_minor_or_node_upgrade]
+
