e674a74feb39e99ddcfd792194f60f455029f70f
Raushan Kumar <raushankr@google.com>
2025-04-30 01:33:23 +0000

    Validated change

--- gke/standard/zonal/dws/main.tf
+++ gke/standard/zonal/dws/main.tf
@@ -1,5 +1,5 @@
 /**
-* Copyright 2024 Google LLC
+* Copyright 2025 Google LLC
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -21,30 +21,36 @@ resource "google_container_cluster" "default" {
   name               = "gke-standard-zonal-flex-cluster"
   location           = "us-central1-a"
   initial_node_count = 1
-
+  min_master_version = "1.32.2-gke.1652000"
+  release_channel {
+    # To use flex-start in GKE, your cluster must use version 1.32.2-gke.1652000 or later.
+    channel = "RAPID"
+  }
   node_config {
     machine_type = "e2-medium"
   }
 }
 # [END gke_standard_zonal_dws_flex_cluster]
 
-
 # [START gke_standard_zonal_dws_non_q_flex]
-resource "google_container_node_pool" "dws_non_q_flex" {
+resource "google_container_node_pool" "default" {
   provider = google-beta
   name     = "gke-standard-zonal-dws-non-q-flex"
   cluster  = google_container_cluster.default.name
   location = google_container_cluster.default.location
 
-  initial_node_count = 0
   autoscaling {
     total_min_node_count = 0
     total_max_node_count = 1
   }
 
+  queued_provisioning {
+    enabled = false
+  }
+
   # More details on usage https://cloud.google.com/kubernetes-engine/docs/how-to/dws-flex-start-training
   node_config {
-    machine_type = "e2-medium"
+    machine_type = "a3-highgpu-8g"
     flex_start   = true
 
     reservation_affinity {
