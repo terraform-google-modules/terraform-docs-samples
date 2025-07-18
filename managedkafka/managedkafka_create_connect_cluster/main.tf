ccaa4d3367b343e99abd98dc8db5631e5d562848
Salman Yousaf <37085288+salmany@users.noreply.github.com>
2025-07-18 20:20:33 +0000

    docs: Add Managed Kafka Connect terraform sample for Clusters
    * Add sample code for creating a cluster

---
+++ managedkafka/managedkafka_create_connect_cluster/main.tf
@@ -0,0 +1,93 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# [START managedkafkaconnect_create_cluster_parent]
+resource "google_managed_kafka_cluster" "default" {
+  project    = data.google_project.default.project_id
+  cluster_id = "my-cluster-id"
+  location   = "us-central1"
+  capacity_config {
+    vcpu_count   = 3
+    memory_bytes = 3221225472
+  }
+  gcp_config {
+    access_config {
+      network_configs {
+        subnet = google_compute_subnetwork.default.id
+      }
+    }
+  }
+}
+
+# [START managedkafka_subnetwork]
+resource "google_compute_subnetwork" "default" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.default.id
+}
+
+
+resource "google_compute_network" "default" {
+  name                    = "test-network"
+  auto_create_subnetworks = false
+}
+# [END managedkafka_subnetwork]
+
+# [START managedkafkaconnect_create_cluster]
+resource "google_managed_kafka_connect_cluster" "example-kafka-connect-cluster" {
+  provider           = google-beta
+  project            = data.google_project.default.project_id # Replace this with your project ID in quotes
+  connect_cluster_id = "my-connect-cluster-id"
+  location           = "us-central1"
+  kafka_cluster      = google_managed_kafka_cluster.default.id
+
+  capacity_config {
+    vcpu_count   = 12
+    memory_bytes = 21474836480
+  }
+
+  gcp_config {
+    access_config {
+      network_configs {
+        primary_subnet = google_compute_subnetwork.default.id
+      }
+    }
+  }
+
+  depends_on = [
+    google_managed_kafka_cluster.example-kafka-cluster
+  ]
+}
+# [END managedkafkaconnect_create_cluster]
+
+# [START managedkafkaconnect_subnetwork]
+resource "google_compute_subnetwork" "default" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
+  network       = google_compute_network.default.id
+}
+
+resource "google_compute_network" "default" {
+  name                    = "test-network"
+  auto_create_subnetworks = false
+}
+# [END managedkafkaconnect_subnetwork]
+
+data "google_project" "default" {
+}
+# [END managedkafkaconnect_create_cluster_parent]
