04945f18117754f2bedcb6d3039d6944bcdb8375
Phil Sung <philbert@gmail.com>
2025-08-14 16:29:11 -0700

    feat(cloud_sql): add MySQL and PG samples for Read Pool (#886)
    * Create test.yaml
    
    Add a `test.yaml` file for the `mysql_instance_replica_pool/main.tf` file.
    
    * Create main.tf
    
    Create `mysql_instance_replica_pool` folder.
    
    Add a MySQL `main.tf` file with Terraform samples related to  the read pool feature. Operations include the following:
    
    * Create a read pool
    * Convert a read replica to a read pool
    * Convert a read pool to a read replica
    * Update read pool node count
    * Edit read pool configuration
    
    Add a corresponding `test.yaml` file.
    
    * Create test.yaml
    
    * Create main.tf
    
    * Delete cloud_sql/mysql_instance_replica_pool/test.yaml
    
    * Delete mysql_instance_replica_pool/main.tf
    
    * Create main.tf
    
    Create `postgres_instance_read_pool` folder.
    
    Add a PostgreSQL `main.tf` file with Terraform samples related to  the read pool feature. Operations include the following:
    
    * Create a read pool
    * Convert a read replica to a read pool
    * Convert a read pool to a read replica
    * Update read pool node count
    * Edit read pool configuration
    
    Add a corresponding `test.yaml` file.
    
    * Create test.yaml
    
    * Retain only the creation sample
    
    * Move samples to cloud_sql subdir
    
    * Remove 'create' from names as it's implied
    
    * terraform fmt
    
    * remove CI aroundarounds, update metadata name
    
    * Update database versions to latest
    
    ---------
    
    Co-authored-by: Rebecca Peterson <44721098+rebecca-pete@users.noreply.github.com>
    Co-authored-by: Katie McLaughlin <glasnt@google.com>
    Co-authored-by: Katie McLaughlin <katie@glasnt.com>

---
+++ cloud_sql/mysql_instance_read_pool/main.tf
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# [START cloud_sql_mysql_read_pool]
+
+resource "google_sql_database_instance" "primary" {
+  name             = "mysql-primary"
+  database_version = "MYSQL_8_4"
+  region           = "europe-west4"
+
+  instance_type = "CLOUD_SQL_INSTANCE"
+
+  settings {
+    tier    = "db-perf-optimized-N-2"
+    edition = "ENTERPRISE_PLUS"
+
+    backup_configuration {
+      enabled            = true
+      binary_log_enabled = true
+    }
+
+    ip_configuration {
+      ipv4_enabled = true
+    }
+  }
+}
+
+resource "google_sql_database_instance" "replica" {
+  name             = "mysql-replica"
+  database_version = "MYSQL_8_4"
+  region           = "europe-west4"
+
+  master_instance_name = google_sql_database_instance.primary.name
+  instance_type        = "READ_POOL_INSTANCE"
+  node_count           = 2
+
+  settings {
+    tier    = "db-perf-optimized-N-2"
+    edition = "ENTERPRISE_PLUS"
+
+    ip_configuration {
+      ipv4_enabled = true
+    }
+  }
+}
+
+# [END cloud_sql_mysql_read_pool]
