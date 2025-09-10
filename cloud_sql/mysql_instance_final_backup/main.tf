b5c64679c489bd660151e98499019ff03f7d8422
Jiongxin Ye <yjiongxin@google.com>
2025-09-10 14:20:57 -0700

    Add Terraform sample for cloud sql final backup config

---
+++ cloud_sql/mysql_instance_final_backup/main.tf
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# [START cloud_sql_mysql_instance_final_backup]
+resource "google_sql_database_instance" "default" {
+  name             = "mysql-instance-final-backup"
+  region           = "asia-northeast1"
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+    final_backup_config {
+      enabled        = true
+      retention_days = 10
+    }
+  }
+  final_backup_description = "TF Sample final backup description"
+}
+# [END cloud_sql_mysql_instance_final_backup]
