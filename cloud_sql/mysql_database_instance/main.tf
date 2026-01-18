60d71b77fcb40cba1ab3044295fad533a9c8a08d
aniketkumarj <112697326+aniketkumarj@users.noreply.github.com>
2026-01-19 03:34:48 +0530

    fix(cloud_sql): Change the deletion_protection flag to true for the Cloud SQL Database sample scripts (#917)
    * Change the deletion_protection flag to true for terraform sample scripts for cloudsql databases.
    
    * Removed the deletion_protection flag from sample scripts which will make it's value to true by default
    
    * update resource identifier
    
    * update default resource id
    
    * update resource ID
    
    ---------
    
    Co-authored-by: Katie McLaughlin <glasnt@google.com>

--- cloud_sql/mysql_database_instance/main.tf
+++ cloud_sql/mysql_database_instance/main.tf
@@ -15,16 +15,13 @@
  */
 
 # [START cloud_sql_mysql_instance_80_db_n1_s2]
-resource "google_sql_database_instance" "instance" {
+resource "google_sql_database_instance" "default" {
   name             = "mysql-instance"
   region           = "us-central1"
   database_version = "MYSQL_8_0"
   settings {
     tier = "db-n1-standard-2"
   }
-  # set `deletion_protection` to true, will ensure that one cannot accidentally delete this instance by
-  # use of Terraform whereas `deletion_protection_enabled` flag protects this instance at the GCP level.
-  deletion_protection = false
 }
 # [END cloud_sql_mysql_instance_80_db_n1_s2]
 
@@ -36,7 +33,7 @@ resource "random_password" "pwd" {
 
 resource "google_sql_user" "user" {
   name     = "user"
-  instance = google_sql_database_instance.instance.name
+  instance = google_sql_database_instance.default.name
   password = random_password.pwd.result
 }
 # [END cloud_sql_mysql_instance_user]
