f815af6f7d4ad4056c2d30427de2a507c9de00b4
Katie McLaughlin <glasnt@google.com>
2026-01-19 08:23:54 +1100

    update default resource id

--- cloud_sql/postgres_database_instance/main.tf
+++ cloud_sql/postgres_database_instance/main.tf
@@ -15,7 +15,7 @@
  */
 
 # [START cloud_sql_postgres_instance_80_db_n1_s2]
-resource "google_sql_database_instance" "instance" {
+resource "google_sql_database_instance" "default" {
   name             = "postgres-instance"
   region           = "us-central1"
   database_version = "POSTGRES_14"
@@ -33,7 +33,7 @@ resource "random_password" "pwd" {
 
 resource "google_sql_user" "user" {
   name     = "user"
-  instance = google_sql_database_instance.instance.name
+  instance = google_sql_database_instance.default.name
   password = random_password.pwd.result
 }
 # [END cloud_sql_postgres_instance_user]
