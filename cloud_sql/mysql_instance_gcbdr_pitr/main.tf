f1dc36b233da96ddc458af018a84e63bf2bde838
aditikumarii-google <aditikumarii@google.com>
2026-01-21 08:52:30 +0530

    Added examples for creating/updating instance using backupdr_backup a… (#913)
    * Added examples for creating/updating instance using backupdr_backup and cloning instance using gcbdr datasource
    
    * corrected lints
    
    * new line at end of gcbdr_pitr
    
    * Skipping tests for documentation
    
    * Changes to resolve comments
    
    * Correcting lint
    
    * Correcting lint
    
    * Changes suggested in comments
    
    * linting corrections
    
    * Changes suggested in comments for bpa
    
    * lint check cleanup (partial)
    
    * Apply suggestion from @glasnt
    
    * Apply suggestion from @glasnt
    
    * Resolve "Value for unconfigurable attribute" error
    
    * Resolve "Value for unconfigurable attribute" error
    
    * reference data resource
    
    ---------
    
    Co-authored-by: Katie McLaughlin <katie@glasnt.com>

---
+++ cloud_sql/mysql_instance_gcbdr_pitr/main.tf
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+# [START cloud_sql_mysql_instance_backupdr_backup_plan_association_setup]
+data "google_backup_dr_backup_plan_association" "association" {
+  # Replace placeholders to reflect the existing backup plan association in the identified project
+  project                    = "PROJECT-ID"
+  location                   = "us-central1"
+  backup_plan_association_id = "BACKUP-PLAN-ASSOCIATION-ID"
+}
+# [END cloud_sql_mysql_instance_backupdr_backup_plan_association_setup]
+
+# [START cloud_sql_mysql_instance_gcbdr_pitr]
+
+data "google_project" "project" {}
+
+resource "google_sql_database_instance" "default" {
+  name             = "mysql-instance-backup-pitr"
+  region           = "us-central1"
+  database_version = "MYSQL_8_0"
+  root_password    = "INSERT-PASSWORD-HERE"
+  settings {
+    tier = "db-custom-2-7680"
+    backup_configuration {
+      enabled    = true
+      start_time = "20:55"
+    }
+  }
+  point_in_time_restore_context {
+    datasource      = data.google_backup_dr_backup_plan_association.association.data_source
+    point_in_time   = "2025-12-22T08:51:50Z" # Replace with the point in time to restore to.
+    target_instance = "${data.google_project.project.project_id}:mysql-instance-backup-pitr"
+  }
+}
+# [END cloud_sql_mysql_instance_gcbdr_pitr]
