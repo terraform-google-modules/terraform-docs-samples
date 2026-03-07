86590e3c716fa00f2afa7a96a6462570e2c25fe6
Jeremy Edwards <1312331+jeremyje@users.noreply.github.com>
2026-03-07 01:38:53 +0000

    docs: Add Terraform configuration for SQL Server Read Pool support example

---
+++ cloud_sql/sqlserver_instance_read_pool/main.tf
@@ -0,0 +1,61 @@
+/**
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# [START cloud_sql_sqlserver_read_pool]
+
+resource "google_sql_database_instance" "primary" {
+  name             = "sqlserver-primary"
+  database_version = "SQLSERVER_2019_ENTERPRISE"
+  region           = "europe-west4"
+  root_password    = "INSERT-PASSWORD-HERE"
+
+  instance_type = "CLOUD_SQL_INSTANCE"
+
+  settings {
+    tier    = "db-perf-optimized-N-2"
+    edition = "ENTERPRISE_PLUS"
+
+    backup_configuration {
+      enabled            = true
+      binary_log_enabled = true
+    }
+
+    ip_configuration {
+      ipv4_enabled = true
+    }
+  }
+}
+
+resource "google_sql_database_instance" "replica" {
+  name             = "sqlserver-replica"
+  database_version = "SQLSERVER_2019_ENTERPRISE"
+  region           = "europe-west4"
+
+  master_instance_name = google_sql_database_instance.primary.name
+  instance_type        = "READ_POOL_INSTANCE"
+  node_count           = 2
+
+  settings {
+    tier    = "db-perf-optimized-N-2"
+    edition = "ENTERPRISE_PLUS"
+
+    ip_configuration {
+      ipv4_enabled = true
+    }
+  }
+}
+
+# [END cloud_sql_sqlserver_read_pool]
