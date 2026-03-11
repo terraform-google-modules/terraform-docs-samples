0fda3b37c3146d1525272de5df97955b6bcda49e
Katie McLaughlin <glasnt@google.com>
2026-03-11 11:38:24 +1100

    update: add additional permission for bigquery connection
    b/489329353

--- bigquery/bigquery_create_connection_cloud_resource/main.tf
+++ bigquery/bigquery_create_connection_cloud_resource/main.tf
@@ -30,10 +30,22 @@ resource "google_bigquery_connection" "default" {
 }
 # [END bigquery_create_connection_cloud_resource]
 
+# [START bigquery_create_connection_cloud_resource_iam_storage]
 ## This grants IAM role access to the service account of the connection created in the previous step.
-resource "google_project_iam_member" "connectionPermissionGrant" {
+resource "google_project_iam_member" "storagepermission" {
   project = data.google_project.default.project_id
   role    = "roles/storage.objectViewer"
   member  = "serviceAccount:${google_bigquery_connection.default.cloud_resource[0].service_account_id}"
 }
+# [END bigquery_create_connection_cloud_resource_iam_storage]
 # [END bigquery_create_connection_cloud_resource_iam]
+
+
+# [START bigquery_create_connection_cloud_resource_iam_storage]
+## This grants IAM role access to the service account of the connection created in the previous step.
+resource "google_project_iam_member" "aiplatformpermission" {
+  project = data.google_project.default.project_id
+  role    = "roles/aiplatform.user"
+  member  = "serviceAccount:${google_bigquery_connection.default.cloud_resource[0].service_account_id}"
+}
+# [END bigquery_create_connection_cloud_resource_iam_storage]
