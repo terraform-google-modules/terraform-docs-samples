fba91c98e849a5bbde65dd842e96ccb1eab51a47
Kevin Wong <kevwongg@google.com>
2026-04-16 10:24:51 -0700

    Update code sample to grant all roles for bigquery

--- workloadidentity/generate_service_agents/main.tf
+++ workloadidentity/generate_service_agents/main.tf
@@ -7,28 +7,25 @@ the `default` project, then grant roles to the service agents.
 data "google_project" "default" {
 }
 
-# Create all project-level aiplatform.googleapis.com service agents
-
+# Create all project-level bigquery.googleapis.com service agents
 resource "google_workload_identity_service_agent" "primary" {
-  parent = "projects/${data.google_project.default.number}/locations/global/serviceProducers/aiplatform.googleapis.com"
+  parent = "projects/${data.google_project.default.number}/locations/global/serviceProducers/bigquery.googleapis.com"
 }
 # [END iam_create_project_level_service_agent]
 
 # [START iam_grant_roles_to_service_agents]
-# Grant the AI Platform Custom Code Service Account the Vertex AI Custom
-# Code Service Agent role (roles/aiplatform.customCodeServiceAgent)
-resource "google_project_iam_member" "custom_code" {
-  project = data.google_project.default.project_id
-  role    = "roles/aiplatform.customCodeServiceAgent"
-  member  = "serviceAccount:service-${data.google_project.default.number}@gcp-sa-aiplatform-cc.iam.gserviceaccount.com"
+locals {
+  service_agent_bindings = {
+    for agent in google_workload_identity_service_agent.primary.service_agents :
+    agent.role => agent.principal... if try(agent.role, null) != null
+  }
 }
 
-# Grant the primary aiplatform.googleapis.com service agent (AI Platform
-# Service Agent) the Vertex AI Service Agent role
-# (roles/aiplatform.serviceAgent)
-resource "google_project_iam_member" "primary" {
-  project = data.google_project.default.project_id
-  role    = "roles/aiplatform.serviceAgent"
-  member  = "serviceAccount:service-${data.google_project.default.number}@gcp-sa-aiplatform.iam.gserviceaccount.com"
+# Grant roles to BigQuery service agents
+resource "google_project_iam_member" "service_agents" {
+  for_each = local.service_agent_bindings
+  project  = data.google_project.default.project_id
+  role     = each.key
+  members  = each.value
 }
 # [END iam_grant_roles_to_service_agents]
