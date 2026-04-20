1e701267e06071b4cba10a85c4edb65d089613cc
Kevin Wong <kevwongg@google.com>
2026-04-20 12:07:42 -0700

    address comments from review

--- workloadidentity/generate_service_agents/main.tf
+++ workloadidentity/generate_service_agents/main.tf
@@ -1,42 +1,36 @@
+/**
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /*
 Create all service agents for aiplatform.googleapis.com for
 the `default` project, then grant roles to the service agents.
 */
 
-# [START iam_create_project_level_service_agent]
+#[START iam_workload_identity_bigquery_service_agents]
 data "google_project" "default" {
 }
 
-# Define a folder
-data "google_folder" "default" {
-  folder = "folders/123456789"
-}
-
-# Define an organization
-data "google_organization" "default" {
-  organization = "organizations/6781234567"
-}
-
-# Create all project-level bigquery.googleapis.com service agents
+#Create all project-level bigquery.googleapis.com service agents
 resource "google_workload_identity_service_agent" "primary" {
   parent = "projects/${data.google_project.default.number}/locations/global/serviceProducers/bigquery.googleapis.com"
 }
+#[END iam_workload_identity_bigquery_service_agents]
 
-# Create all folder-level accessapproval.googleapis.com service agents
-resource "google_workload_identity_service_agent" "primaryfolder" {
-  parent = "folders/${data.google_folder.default.folder_id}/locations/global/serviceProducers/accessapproval.googleapis.com"
-}
-
-# Create all organization-level accessapproval.googleapis.com service agents
-resource "google_workload_identity_service_agent" "primaryorganization" {
-  parent = "organizations/${data.google_organization.default.org_id}/locations/global/serviceProducers/accessapproval.googleapis.com"
-}
-
-
-# [END iam_create_project_level_service_agent]
-
-# [START iam_grant_roles_to_service_agents]
-# Grant roles to BigQuery service agents for project
+#[START iam_workload_identity_bigquery_iam_member]
+#Grant roles to BigQuery service agents for project
 resource "google_project_iam_member" "service_agents" {
   for_each = {
     for i, agent in google_workload_identity_service_agent.primary.service_agents :
@@ -46,4 +40,4 @@ resource "google_project_iam_member" "service_agents" {
   role    = each.value.role
   member  = each.value.principal
 }
-# [END iam_grant_roles_to_service_agents]
+#[END iam_workload_identity_bigquery_iam_member]
