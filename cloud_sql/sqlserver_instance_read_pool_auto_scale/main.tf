7fdcc62d243a03a8f1ecdccdd114798613524831
Jeel Christian <jeelchristian@google.com>
2026-08-06 00:16:31 +0000

    feat(cloud_sql): add sqlserver_instance_read_pool_auto_scale sample

---
+++ cloud_sql/sqlserver_instance_read_pool_auto_scale/main.tf
@@ -0,0 +1,76 @@
+/**
+ * Copyright 2026 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# [START cloud_sql_sqlserver_read_pool_auto_scale]
+
+resource "google_sql_database_instance" "primary" {
+  name             = "sqlserver-primary"
+  database_version = "SQLSERVER_2022_ENTERPRISE"
+  region           = "europe-west4"
+  instance_type    = "CLOUD_SQL_INSTANCE"
+
+  settings {
+    tier    = "db-perf-optimized-N-2"
+    edition = "ENTERPRISE_PLUS"
+
+    backup_configuration {
+      enabled                        = true
+      point_in_time_recovery_enabled = true
+    }
+
+    ip_configuration {
+      ipv4_enabled = true
+    }
+  }
+
+  deletion_protection = false
+}
+
+resource "google_sql_database_instance" "read_pool" {
+  name                 = "sqlserver-read-pool"
+  database_version     = "SQLSERVER_2022_ENTERPRISE"
+  region               = "europe-west4"
+  master_instance_name = google_sql_database_instance.primary.name
+  instance_type        = "READ_POOL_INSTANCE"
+  node_count           = 2
+
+  settings {
+    tier    = "db-perf-optimized-N-2"
+    edition = "ENTERPRISE_PLUS"
+
+    ip_configuration {
+      ipv4_enabled = true
+    }
+
+    read_pool_auto_scale_config {
+      enabled                    = true
+      min_node_count             = 2
+      max_node_count             = 5
+      disable_scale_in           = false
+      scale_in_cooldown_seconds  = 600
+      scale_out_cooldown_seconds = 600
+
+      target_metrics {
+        metric       = "AVERAGE_CPU_UTILIZATION"
+        target_value = 0.5
+      }
+    }
+  }
+
+  deletion_protection = false
+}
+
+# [END cloud_sql_sqlserver_read_pool_auto_scale]
