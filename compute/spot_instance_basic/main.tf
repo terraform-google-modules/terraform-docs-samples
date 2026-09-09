43cbe69abe7d5acd0f81e730bc560d02a1cabada
Savija <savijatv@google.com>
2026-09-09 16:55:38 +0200

    Update main.tf
    Debian 11 has reached EOS in August 2026 (https://docs.cloud.google.com/compute/docs/images/os-details#expandable-3). Updating the sample to use Debian 13 image.

--- compute/spot_instance_basic/main.tf
+++ compute/spot_instance_basic/main.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022 Google LLC
+ * Copyright 2026 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ resource "google_compute_instance" "spot_vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "debian-cloud/debian-13"
     }
   }
 
